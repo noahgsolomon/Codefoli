4c3cd2469a787890f948bd4f7789635d6d89b58d
Noah Solomon <noahsolomon2003@gmail.com>
2023-06-06 21:02:18 -0400

    login register home styling done.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import {Link} from "react-router-dom";
 
 const Login: React.FC = () => {
+    const [showFields, setShowFields] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
         <div className="login-card">
-                <h2 className="login-header">Codefolio</h2>
-                <p className="login-text">Log in to your account</p>
-                <button className="login-button github">Continue with GitHub</button>
-                <button className="login-button email">Continue with Email and Password</button>
-            <Link to={'/register'}><p className="login-link">Create your account</p></Link>
+            <h2 className="login-header">Codefolio</h2>
+            <p className="login-text">Log in to your account</p>
+            {showFields ? (
+                <>
+                    <button
+                        className="back-button"
+                        onClick={() => setShowFields(false)}
+                    >
+                        Back
+                    </button>
+                    <input
+                        type="email"
+                        className="form-field"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        className="form-field"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button className={`login-button ${email.length > 3 && password.length > 3 ? 'active' : ''}`}>Log In</button>
+                </>
+            ) : (
+                <>
+                    <button className="login-button github">Continue with GitHub</button>
+                    <button
+                        className="login-button email"
+                        onClick={() => setShowFields(true)}
+                    >
+                        Continue with Email and Password
+                    </button>
+                    <Link to={'/register'}>
+                        <p className="login-link">Create your account</p>
+                    </Link>
+                </>
+            )}
         </div>
     )
 }
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import './style.css';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Register: React.FC = () => {
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div className="login-card">
             <h2 className="login-header">Register for free!</h2>
             <form>
-                <input type="text" className="form-field" placeholder="Full Name" />
-                <input type="email" className="form-field" placeholder="Email" />
-                <input type="password" className="form-field" placeholder="Password" />
+                <input
+                    type="text"
+                    className="form-field"
+                    placeholder="Full Name"
+                    value={fullName}
+                    onChange={(e) => setFullName(e.target.value)}
+                />
+                <input
+                    type="email"
+                    className="form-field"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    className="form-field"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
             </form>
+            <button
+                className={`login-button ${fullName.length > 4 && email.length > 4 && password.length > 4 ? 'active' : ''}`}
+                disabled={!(fullName.length > 4 && email.length > 4 && password.length > 4)}
+            >
+                Register
+            </button>
             <p className="login-text">or</p>
             <button className="login-button github">Continue with GitHub</button>
             <Link to={'/login'}><p className="login-link">Already have an account? Log in</p></Link>
