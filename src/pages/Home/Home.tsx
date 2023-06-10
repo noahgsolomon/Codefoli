85eaac9aea013a4ef7d58be10498b20f38eab2d0
Noah Solomon <noahsolomon2003@gmail.com>
2023-06-10 10:47:48 -0400

    home page style revamp

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,13 @@
 import React, {useEffect} from 'react';
 import { useSpring, animated } from 'react-spring';
-import { FiArrowUpRight, FiUsers, FiCode, FiRefreshCw } from 'react-icons/fi';
 import './style.css';
 import {useNavigate} from "react-router-dom";
+import { FiCode, FiRefreshCw, FiLayout } from 'react-icons/fi';
 
 const Home: React.FC = () => {
 
     const navigate = useNavigate();
-    
+
     useEffect(() => {
         if (localStorage.getItem('loggedIn') === 'true') {
             navigate('/dashboard');
@@ -24,43 +24,46 @@ const Home: React.FC = () => {
         <animated.div style={fadeIn} className="home-container">
             <main>
                 <section className="intro-section">
-                    <h2 className={'icon'}>Welcome to <span>Codefolio</span>! <FiArrowUpRight/></h2>
-                    <p data-tip="About Codefolio"><span>Codefolio</span> is a platform dedicated to helping programmers create stunning portfolio websites. We believe in the power of showcasing your work effectively, and our mission is to make this process as simple as possible for you. With <span>Codefolio</span>, you can:</p>
+                    <h1>Welcome to <span className='highlight-codefolio'>Codefolio</span>!</h1>
+                    <img className="intro-image" src="src/assets/profiledisplay.png" alt="Introductory visual"/>
+                    <p>Codefolio is a platform dedicated to helping programmers create stunning portfolio websites...</p>
+                    <h2>What you can do:</h2>
                     <div className="ide-list">
-                        <div className="bracket">{`{`}</div>
-                        <ul style={{listStyleType: 'none'}}>
-                            <li><FiUsers/> Create a professional and personalized portfolio website</li>
-                            <li><FiCode/> Showcase your projects, skills, and accomplishments</li>
-                            <li><FiRefreshCw/> Easily update and maintain your portfolio over time</li>
+                        <ul>
+                            <li>Create a professional and personalized portfolio website</li>
+                            <li>Showcase your projects, skills, and accomplishments</li>
+                            <li>Easily update and maintain your portfolio over time</li>
                         </ul>
-                        <div className="bracket">{`}`}</div>
                     </div>
-                    <p data-tip="Get Started">Are you ready to build your portfolio website with <span>Codefolio</span>? Click the "Sign Up" button above to create your account and get started!</p>
+                    <h3>Ready to build your portfolio website with Codefolio? Sign Up and get started!</h3>
                 </section>
                 <section className="testimonial-section">
-                    <h2 className={'icon'}>What Our <span>Users</span> Say <FiUsers/></h2>
-                    <blockquote>
-                        "Codefolio made building my portfolio a breeze. The user-friendly interface and the customizable templates helped me create a portfolio that truly represents my skills and work."
-                        <cite>- Jane Doe</cite>
-                    </blockquote>
-                    <blockquote>
-                        "With Codefolio, I was able to create a professional portfolio in no time. The project showcasing feature is excellent, and it helped me highlight my work effectively."
-                        <cite>- John Smith</cite>
-                    </blockquote>
+                    <h2>What Our <span className='highlight-users'>Users</span> Say</h2>
+                    <div className="testimonial-grid">
+                        <blockquote>"Codefolio made building my portfolio a breeze. It truly represents my skills and work." - Jane Doe</blockquote>
+                        <img className={'profile-img'} src="src/assets/user2-pfp.png" alt="User testimonial"/>
+                        <img className={'profile-img'} src="src/assets/user-pfp.png" alt="User testimonial"/>
+                        <blockquote>"With Codefolio, I was able to create a professional portfolio in no time." - John Smith</blockquote>
+                    </div>
                 </section>
                 <section className="feature-section">
-                    <h2 className={'icon'}><span>Codefolio</span> Features <FiCode/></h2>
-                    <div>
-                        <h3>Customizable Templates <FiRefreshCw/></h3>
-                        <p data-tip="Customize your portfolio">Choose from a variety of templates that suit your style and personalize them according to your needs.</p>
-                    </div>
-                    <div>
-                        <h3>Project Showcasing <FiCode/></h3>
-                        <p data-tip="Showcase your projects">Highlight your projects effectively with detailed descriptions, code snippets, and live demo links.</p>
-                    </div>
-                    <div>
-                        <h3>Easy Updates <FiRefreshCw/></h3>
-                        <p data-tip="Keep your portfolio updated">Keep your portfolio up-to-date with our easy-to-use interface. Add new projects, update your skills, and edit your details whenever you want.</p>
+                    <h2>Codefolio <span className='highlight-features'>Features</span></h2>
+                    <div className="features-grid">
+                        <div>
+                            <FiLayout size={40} />
+                            <h3>Customizable Templates</h3>
+                            <p>Choose from a variety of templates that suit your style and personalize them according to your needs.</p>
+                        </div>
+                        <div>
+                            <FiCode size={40} />
+                            <h3>Project Showcasing</h3>
+                            <p>Highlight your projects effectively with detailed descriptions, code snippets, and live demo links.</p>
+                        </div>
+                        <div>
+                            <FiRefreshCw size={40} />
+                            <h3>Easy Updates</h3>
+                            <p>Keep your portfolio up-to-date with our easy-to-use interface.</p>
+                        </div>
                     </div>
                 </section>
             </main>
