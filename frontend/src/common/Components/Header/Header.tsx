f2bda10123892d6a93ca7a983fd220621444ebe4
Noah Solomon <noahsolomon2003@gmail.com>
2023-06-17 13:42:54 -0400

    header formatting for smaller screens

--- frontend/src/common/Components/Header/Header.tsx
+++ frontend/src/common/Components/Header/Header.tsx
@@ -13,68 +13,68 @@ const Header: React.FC = () => {
   }, []);
 
   return (
-    <header
-      className={`relative z-40 flex items-center justify-center py-5 transition-opacity duration-300 ease-linear ${
-        loading ? "invisible opacity-0" : "visible opacity-100"
-      }`}
-    >
-      <div
-        className="mx-auto flex w-full items-center justify-between rounded-xl border-2 border-black bg-white px-4 py-3 shadow-custom transition-all duration-300 ease-linear"
-        style={{ maxWidth: "50rem" }}
+      <header
+          className={`mx-5 relative z-40 flex flex-col md:flex-row items-center justify-center py-5 transition-opacity duration-300 ease-linear ${
+              loading ? "invisible opacity-0" : "visible opacity-100"
+          }`}
       >
-        <h1 className="cursor-pointer select-none text-4xl transition-all duration-300 ease-linear hover:text-blue-500">
-          <Link to={logoLink} className="text-current no-underline">
-            Codefolio
-          </Link>
-        </h1>
-        {blank ? (
-          <></>
-        ) : showButtons ? (
-          <>
-            <div className="flex flex-grow items-center justify-center text-xl font-bold text-gray-800 transition-all duration-300 ease-linear">
+        <div
+            className="mx-auto flex flex-col md:flex-row w-full items-center justify-between rounded-xl border-2 border-black bg-white px-4 py-3 shadow-custom transition-all duration-300 ease-linear"
+            style={{ maxWidth: "50rem" }}
+        >
+          <h1 className="cursor-pointer select-none transition-all duration-300 ease-linear hover:text-blue-500">
+            <Link to={logoLink} className="text-4xl md:text-2xl text-current no-underline">
+              Codefolio
+            </Link>
+          </h1>
+          {blank ? (
+              <></>
+          ) : showButtons ? (
+              <>
+                <div className="flex flex-row md:flex-row items-center justify-between w-full md:text-base font-bold text-gray-800 duration-300 ease-linear">
+                  <Link
+                      to="/dashboard"
+                      className="mx-2 md:mx-10 py-1 no-underline transition-all text-lg duration-300 ease-linear hover:text-blue-500"
+                  >
+                    Dashboard
+                  </Link>
+                  <Link
+                      to="/about"
+                      className="mx-2 md:mx-10 py-1 no-underline transition-all text-lg duration-300 ease-linear hover:text-blue-500"
+                  >
+                    About
+                  </Link>
+                  <Link
+                      to="/portfolio"
+                      className="mx-2 md:mx-10 py-1 no-underline transition-all text-lg duration-300 ease-linear hover:text-blue-500"
+                  >
+                    Portfolio
+                  </Link>
+                  <a
+                      href="mailto:email@example.com"
+                      className="my-2 md:my-0 md:ml-2 flex items-center justify-center rounded-full bg-black px-3 py-2 text-white transition-all duration-300 ease-linear hover:-translate-y-0.5 hover:bg-blue-500"
+                  >
+                    <img
+                        className={"w-8 transition-all duration-300 ease-linear"}
+                        src={"src/assets/email.svg"}
+                        alt={"email icon"}
+                    />
+                  </a>
+                </div>
+              </>
+          ) : (
               <Link
-                to="/dashboard"
-                className="mx-10 py-1 no-underline transition-all duration-300 ease-linear hover:text-blue-500"
+                  to="/login"
+                  className="my-2 md:my-0 text-current no-underline transition-all duration-300 ease-linear"
               >
-                Dashboard
-              </Link>
-              <Link
-                to="/about"
-                className="mx-10 py-1 no-underline transition-all duration-300 ease-linear hover:text-blue-500"
-              >
-                About
-              </Link>
-              <Link
-                to="/portfolio"
-                className="mx-10 py-1 no-underline transition-all duration-300 ease-linear hover:text-blue-500"
-              >
-                Portfolio
-              </Link>
-            </div>
-            <a
-              href="mailto:email@example.com"
-              className="ml-2 flex items-center justify-center rounded-full bg-black px-3 py-2 text-white transition-all duration-300 ease-linear hover:-translate-y-0.5 hover:bg-blue-500"
-            >
-              <img
-                className={"w-8 transition-all duration-300 ease-linear"}
-                src={"src/assets/email.svg"}
-                alt={"email icon"}
-              />
-            </a>
-          </>
-        ) : (
-          <Link
-            to="/login"
-            className="text-current no-underline transition-all duration-300 ease-linear"
-          >
-            <button className="duration-400 inline-block cursor-pointer rounded-full bg-black px-6 py-2 text-center font-custom text-lg text-white no-underline transition-all ease-linear hover:translate-y-[-5px] hover:bg-blue-500">
+                <button className="duration-400 inline-block cursor-pointer rounded-full bg-black px-6 py-2 text-center font-custom text-lg text-white no-underline transition-all ease-linear hover:translate-y-[-5px] hover:bg-blue-500">
 
-              Hop In!
-            </button>
-          </Link>
-        )}
-      </div>
-    </header>
+                  Hop In!
+                </button>
+              </Link>
+          )}
+        </div>
+      </header>
   );
 };
 
