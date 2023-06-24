5e5d2c8e3d08a7126b94a845323f8c8fb01b895d
Noah Solomon <noahsolomon2003@gmail.com>
2023-06-24 08:22:33 -0400

    About page configured for new data retrieval

--- frontend/src/pages/About/About.tsx
+++ frontend/src/pages/About/About.tsx
@@ -2,16 +2,17 @@ import Marquee from "Components/Marquee/Marquee";
 import JobCard from "./JobCard/JobCard";
 import Footer from "Components/Footer/Footer";
 import Card from "Components/Card/Card";
-import AuthProps from "Type/AuthProps.tsx";
 import React from "react";
-import Loader from "Components/Loader/Loader.tsx";
+import { Link } from "react-router-dom";
+import { ValuesData } from "Type/Values.tsx";
+import UserData from "Type/UserData.tsx";
+import AboutData from "Type/AboutData.tsx";
 
-const About: React.FC<AuthProps> = ({ userData, loading }) => {
+const About: React.FC<{ userData: UserData; pageData: AboutData }> = ({
+  userData,
+  pageData,
+}) => {
   console.log(userData);
-
-  if (loading) {
-    return <Loader />;
-  }
 
   return (
     <>
@@ -21,23 +22,20 @@ const About: React.FC<AuthProps> = ({ userData, loading }) => {
           <section className="about mb-20 grid grid-cols-2 justify-center gap-10 md:h-[400px] md:grid-cols-5">
             <div className="content-wrapper col-span-2  md:order-2 md:col-span-3">
               <h2 className="mb-5 text-center text-5xl font-bold md:text-7xl">
-                Hello, I'm <br />{" "}
-                <span className="mt-1 inline-block bg-blue-500 text-white">
-                  {userData?.name}
-                </span>
+                {pageData.about.headerOne}
               </h2>
             </div>
             <div className="image-wrapper order-2 w-full text-center md:order-1 md:self-end">
               <img
                 className="inline-block max-w-[150px]"
-                src="https://assets.website-files.com/63360c0c2b86f80ba8b5421a/633b440128f648585c383865_about-hero-left-image-paperfolio-webflow-template.png"
+                src={pageData.about.iconOne}
                 alt="portfolio"
               />
             </div>
             <div className="image-wrapper w-full text-center md:order-last md:self-start">
               <img
                 className="inline-block max-w-[150px]"
-                src="https://assets.website-files.com/63360c0c2b86f80ba8b5421a/633b443e2bb8e12b5faf51a7_about-hero-rigth-image-paperfolio-webflow-template.png"
+                src={pageData.about.iconTwo}
                 alt="portfolio"
               />
             </div>
@@ -48,27 +46,23 @@ const About: React.FC<AuthProps> = ({ userData, loading }) => {
           <div className="container mx-auto my-20 max-w-screen-lg gap-5 px-5 md:grid md:grid-cols-2 md:items-start md:justify-between">
             <div className="content-left">
               <h2 className="mb-8 text-center text-4xl font-bold md:text-left md:text-6xl md:leading-tight">
-                My <span className="bg-indigo-600 text-white">story</span> as a
-                designer
+                {pageData.about.headerTwo}
               </h2>
               <div className="image-wrapper mb-5 md:max-w-[375px]">
-                <img
-                  src="https://assets.website-files.com/63360c0c2b86f80ba8b5421a/633b52d3639fb5250039e574_my-story-image-paperfolio-webflow-template.png"
-                  alt=""
-                />
+                <img src={pageData.about.iconThree} alt="" />
               </div>
             </div>
             <div className="content-right">
               <p className="description mb-5 text-lg font-semibold">
-                {userData.about}
+                {pageData.about.descriptionOne}
               </p>
               <div className="story-control text-center md:text-left">
-                <a
-                  href="#"
+                <Link
+                  to="/preview/contact"
                   className="mb-4 mr-2 inline-block w-full rounded-xl border-2 border-transparent bg-black px-4 py-2 font-bold text-white transition ease-in hover:-translate-y-1 hover:bg-blue-500 md:w-auto"
                 >
                   Get in touch
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -77,46 +71,35 @@ const About: React.FC<AuthProps> = ({ userData, loading }) => {
           <div className="container mx-auto my-20 max-w-screen-lg gap-5 px-5 py-20 md:grid md:grid-cols-2 md:items-center md:justify-between">
             <div className="content-left">
               <h2 className="mb-8 text-4xl font-bold text-white md:text-5xl md:leading-tight">
-                Designing since I was{" "}
-                <span className="mt-2 inline-block bg-red-500 text-white">
-                  ? years old
-                </span>
+                {pageData.about.headerThree}
               </h2>
               <p className="description text-lg font-semibold text-white">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi,
-                ipsam illo. Quas, maiores perspiciatis aperiam eum excepturi
-                laudantium hic nisi assumenda adipisci quo quis similique.
+                {pageData.about.descriptionTwo}
               </p>
               <div className="events-wrapper my-5">
                 <div className="event flex items-start justify-between gap-4">
                   <div className="mt-1 h-4 w-4 rounded border-2 bg-indigo-600"></div>
                   <p className="event-descripition flex-1 pt-0 text-lg font-semibold text-white">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Soluta, placeat!
+                    {pageData.about.bulletOne}
                   </p>
                 </div>
                 <div className="event flex items-start justify-between gap-4">
                   <div className="mt-1 h-4 w-4 rounded border-2 bg-sky-600"></div>
                   <p className="event-descripition flex-1 pt-0 text-lg font-semibold text-white">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Soluta, placeat!
+                    {pageData.about.bulletTwo}
                   </p>
                 </div>
                 <div className="event flex items-start justify-between gap-4">
                   <div className="mt-1 h-4 w-4 rounded border-2 bg-yellow-500"></div>
                   <p className="event-descripition flex-1 pt-0 text-lg font-semibold text-white">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Soluta, placeat!
+                    {pageData.about.bulletThree}
                   </p>
                 </div>
               </div>
             </div>
             <div className="content-right">
               <div className="image-wrapper">
-                <img
-                  src="https://assets.website-files.com/63360c0c2b86f80ba8b5421a/633b55bcb4baec57b75b66fd_desigining-experience-paperfolio-webflow-template.png"
-                  alt=""
-                />
+                <img src={pageData.about.imageOne} alt="" />
               </div>
             </div>
           </div>
@@ -125,11 +108,12 @@ const About: React.FC<AuthProps> = ({ userData, loading }) => {
         <section className="resume">
           <div className="container mx-auto my-20 max-w-screen-lg px-5 py-20">
             <h2 className="mb-8 text-center text-3xl font-bold">
-              Take a look at my <span className="bg-yellow-500">resume</span>
+              {pageData.about.headerFour}
             </h2>
             <div className="resume-events">
               {userData.work.map((job, index) => (
                 <JobCard
+                  key={index}
                   companyTitle={job.company}
                   role={job.position}
                   description={job.description}
@@ -152,34 +136,20 @@ const About: React.FC<AuthProps> = ({ userData, loading }) => {
         <section className="services">
           <div className="container mx-auto my-20 max-w-screen-lg px-5 py-20">
             <h2 className="mb-8 text-center text-3xl font-bold">
-              The core values that drive my work
+              {pageData.about.headerFive}
             </h2>
             <p className="desciption mb-8 text-center text-lg font-semibold">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-              saepe in iure alias tenetur sapiente voluptatum assumenda placeat?
-              Veniam, laboriosam.
+              {pageData.about.descriptionThree}
             </p>
             <div className="cards-wrapper flex flex-wrap justify-center gap-5 lg:justify-between">
-              <Card
-                title="Hard Work"
-                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa numquam, sequi corporis quaerat voluptatibus deserunt"
-                imageUrl="https://img.icons8.com/cotton/400/fast-delivery--v1.png"
-              />
-              <Card
-                title="Transparency"
-                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa numquam, sequi corporis quaerat voluptatibus deserunt"
-                imageUrl="https://img.icons8.com/cotton/400/search-property.png"
-              />
-              <Card
-                title="Innovation"
-                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa numquam, sequi corporis quaerat voluptatibus deserunt"
-                imageUrl="https://img.icons8.com/cotton/400/gas-industry.png"
-              />
-              <Card
-                title="Growth"
-                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa numquam, sequi corporis quaerat voluptatibus deserunt"
-                imageUrl="https://img.icons8.com/cotton/400/hand-planting--v1.png"
-              />
+              {pageData.values.map((value, index) => (
+                <Card
+                  key={index}
+                  title={value.value.replaceAll("_", " ")}
+                  description={ValuesData[value.value].description}
+                  imageUrl={ValuesData[value.value].image}
+                />
+              ))}
             </div>
           </div>
         </section>
