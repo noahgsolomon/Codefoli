07841f46073df7571997e9a89013f072dcc43c77
noah s <noahsolomon2003@gmail.com>
2023-07-07 15:04:00 -0400

    standardized page section backend collecting

---
+++ backend/src/main/java/com/codefolio/backend/util/PageSections.java
@@ -0,0 +1,94 @@
+package com.codefolio.backend.util;
+
+import com.codefolio.backend.user.Users;
+import com.codefolio.backend.user.sections.PageType;
+import com.codefolio.backend.user.sections.Section;
+import com.codefolio.backend.user.sections.SectionRepository;
+import com.codefolio.backend.user.sections.type.faq.FAQSection;
+import com.codefolio.backend.user.sections.type.faq.FAQSectionRepository;
+import com.codefolio.backend.user.sections.type.faq.FAQSectionResponseModel;
+import com.codefolio.backend.user.sections.type.resume.ResumeSection;
+import com.codefolio.backend.user.sections.type.resume.ResumeSectionRepository;
+import com.codefolio.backend.user.sections.type.resume.ResumeSectionResponseModel;
+import com.codefolio.backend.user.sections.type.skill.SkillSection;
+import com.codefolio.backend.user.sections.type.skill.SkillSectionRepository;
+import com.codefolio.backend.user.sections.type.skill.SkillSectionResponseModel;
+import com.codefolio.backend.user.sections.type.story.StorySection;
+import com.codefolio.backend.user.sections.type.story.StorySectionRepository;
+import com.codefolio.backend.user.sections.type.story.StorySectionResponseModel;
+import com.codefolio.backend.user.sections.type.value.ValueSection;
+import com.codefolio.backend.user.sections.type.value.ValueSectionRepository;
+import com.codefolio.backend.user.sections.type.value.ValueSectionResponseModel;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@AllArgsConstructor
+public class PageSections {
+
+    private final SectionRepository sectionRepository;
+    private final FAQSectionRepository faqSectionRepository;
+    private final ResumeSectionRepository resumeSectionRepository;
+    private final SkillSectionRepository skillSectionRepository;
+    private final StorySectionRepository storySectionRepository;
+    private final ValueSectionRepository valueSectionRepository;
+
+    public List<Object> getSections(Users user, PageType pageType) {
+        List<Section> sections = sectionRepository.findAllByUsersAndPage(user, pageType);
+        List<Object> sectionDetails = new ArrayList<>();
+
+        for (Section section : sections) {
+            Map<String, Object> map = new HashMap<>();
+
+            switch (section.getType()) {
+                case VALUE -> {
+                    ValueSection valueSection = valueSectionRepository.findByUsers(user).orElse(null);
+                    if (valueSection != null) {
+                        map.put("type", "VALUE");
+                        map.put("details", new ValueSectionResponseModel(valueSection.getHeaderOne(), valueSection.getDescriptionOne()));
+                        sectionDetails.add(map);
+                    }
+                }
+                case STORY -> {
+                    StorySection storySection = storySectionRepository.findByUsers(user).orElse(null);
+                    if (storySection != null) {
+                        map.put("type", "STORY");
+                        map.put("details", new StorySectionResponseModel(storySection.getHeaderOne(), storySection.getDescriptionOne(), storySection.getBulletOne(), storySection.getBulletTwo(), storySection.getBulletThree(), storySection.getImageOne()));
+                        sectionDetails.add(map);
+                    }
+                }
+                case SKILL -> {
+                    SkillSection skillSection = skillSectionRepository.findByUsers(user).orElse(null);
+                    if (skillSection != null) {
+                        map.put("type", "SKILL");
+                        map.put("details", new SkillSectionResponseModel(skillSection.getHeaderOne()));
+                        sectionDetails.add(map);
+                    }
+                }
+                case RESUME -> {
+                    ResumeSection resumeSection = resumeSectionRepository.findByUsers(user).orElse(null);
+                    if (resumeSection != null) {
+                        map.put("type", "RESUME");
+                        map.put("details", new ResumeSectionResponseModel(resumeSection.getHeaderOne()));
+                        sectionDetails.add(map);
+                    }
+                }
+                case FAQ -> {
+                    FAQSection faqSection = faqSectionRepository.findByUsers(user).orElse(null);
+                    if (faqSection != null) {
+                        map.put("type", "FAQ");
+                        map.put("details", new FAQSectionResponseModel(faqSection.getHeaderOne(), faqSection.getDescriptionOne()));
+                        sectionDetails.add(map);
+                    }
+                }
+            }
+        }
+        return sectionDetails;
+    }
+
+}
