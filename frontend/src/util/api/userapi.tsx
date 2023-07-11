b49e32fc3f91be5d98ea5bd84951c058e403450c
noah s <noahsolomon2003@gmail.com>
2023-07-11 15:07:41 -0400

    add service and remove service frontend endpoints

--- frontend/src/util/api/userapi.tsx
+++ frontend/src/util/api/userapi.tsx
@@ -20,6 +20,27 @@ const userDetails = async () => {
   }
 };
 
+const addService = async (service: string) => {
+  try {
+    const response = await fetch("http://localhost:8080/add-service", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: service,
+      credentials: "include",
+    });
+
+    const responseJson = await response.json();
+    if (responseJson.status === "OK") {
+      return responseJson;
+    } else {
+      console.log(responseJson.message);
+      return responseJson;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 const updateService = async (before: string, after: string) => {
   const model = {
     before: before,
@@ -30,6 +51,27 @@ const updateService = async (before: string, after: string) => {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(model),
+      credentials: "include",
+    });
+
+    const responseJson = await response.json();
+    if (responseJson.status === "OK") {
+      return responseJson;
+    } else {
+      console.log(responseJson.message);
+      return responseJson;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const removeService = async (service: string) => {
+  try {
+    const response = await fetch("http://localhost:8080/remove-service", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: service,
       credentials: "include",
     });
 
@@ -201,4 +243,6 @@ export {
   updateService,
   removeLanguage,
   addLanguage,
+  removeService,
+  addService,
 };
