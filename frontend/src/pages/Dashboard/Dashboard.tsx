86d689ca1c2508ade7c5c564a376ccd46f41e50b
noah s <noahsolomon2003@gmail.com>
2023-07-12 17:58:55 -0400

    sections in homepage moved to DashboardSections.tsx

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -5,14 +5,7 @@ import { useSpring, animated } from "react-spring";
 import HomeData from "Type/HomeData.tsx";
 import UserData from "Type/UserData.tsx";
 import { updateDescriptionOne, updateHeaderOne } from "./dashboardapi.tsx";
-import SkillSection from "Components/Sections/Skill/SkillSection.tsx";
-import StorySection from "Components/Sections/Story/StorySection.tsx";
-import ResumeSection from "Components/Sections/Resume/ResumeSection.tsx";
-import AnyPageData from "Type/AnyPageData.tsx";
-import FAQSection from "Components/Sections/FAQSection.tsx";
-import ValueSection from "Components/Sections/ValueSection.tsx";
-import AddSection from "Components/AddSection/AddSection.tsx";
-import { SectionType } from "Type/Section.tsx";
+import DashboardSections from "./DashboardSections.tsx";
 
 const Dashboard: React.FC<{
   pageData: HomeData;
@@ -116,20 +109,6 @@ const Dashboard: React.FC<{
       console.error("Error uploading file: ", error);
     }
   };
-
-  const allSectionTypes: SectionType[] = [
-    "STORY",
-    "RESUME",
-    "SKILL",
-    "FAQ",
-    "VALUE",
-  ] as SectionType[];
-
-  const sectionList = pageData.sections.map((section) => section.type);
-
-  const availableSectionTypes = allSectionTypes.filter(
-    (t) => !sectionList.includes(t)
-  );
 
   console.log(pageData);
 
@@ -241,129 +220,11 @@ const Dashboard: React.FC<{
             </div>
           </div>
         </div>
-        {pageData.sections
-          .sort((a, b) => a.details.order - b.details.order)
-          .map((section, index) => {
-            const { type, details } = section;
-
-            let sectionComponent;
-            switch (type) {
-              case "SKILL":
-                sectionComponent = (
-                  <SkillSection
-                    key={index}
-                    userData={userData}
-                    setUserData={setUserData}
-                    preview={false}
-                    details={details}
-                    setPageData={
-                      setPageData as React.Dispatch<
-                        React.SetStateAction<AnyPageData>
-                      >
-                    }
-                    page={"HOME"}
-                    order={section.details.order}
-                  />
-                );
-                break;
-              case "STORY":
-                sectionComponent =
-                  "descriptionOne" in details &&
-                  "bulletOne" in details &&
-                  "bulletTwo" in details &&
-                  "bulletThree" in details &&
-                  "imageOne" in details ? (
-                    <StorySection
-                      page={"HOME"}
-                      key={index}
-                      details={details}
-                      setPageData={
-                        setPageData as React.Dispatch<
-                          React.SetStateAction<AnyPageData>
-                        >
-                      }
-                      order={section.details.order}
-                    />
-                  ) : null;
-                break;
-              case "RESUME":
-                sectionComponent = (
-                  <ResumeSection
-                    key={index}
-                    page={"HOME"}
-                    details={details}
-                    setPageData={
-                      setPageData as React.Dispatch<
-                        React.SetStateAction<AnyPageData>
-                      >
-                    }
-                    userData={userData}
-                    order={section.details.order}
-                  />
-                );
-                break;
-              case "FAQ":
-                sectionComponent =
-                  "descriptionOne" in details &&
-                  "headerOne" in details &&
-                  "faq" in details ? (
-                    <FAQSection
-                      setPageData={
-                        setPageData as React.Dispatch<
-                          React.SetStateAction<AnyPageData>
-                        >
-                      }
-                      key={index}
-                      page={"HOME"}
-                      details={details}
-                      order={section.details.order}
-                    />
-                  ) : null;
-                break;
-              case "VALUE":
-                sectionComponent =
-                  "descriptionOne" in details &&
-                  "headerOne" in details &&
-                  "values" in details ? (
-                    <ValueSection
-                      setPageData={
-                        setPageData as React.Dispatch<
-                          React.SetStateAction<AnyPageData>
-                        >
-                      }
-                      key={index}
-                      page={"HOME"}
-                      details={details}
-                      order={section.details.order}
-                    />
-                  ) : null;
-                break;
-              default:
-                sectionComponent = null;
-            }
-
-            return [
-              <AddSection
-                key={`add-${index}`}
-                sections={availableSectionTypes}
-                page={"HOME"}
-                setPageData={
-                  setPageData as React.Dispatch<
-                    React.SetStateAction<AnyPageData>
-                  >
-                }
-                order={section.details.order}
-              />,
-              sectionComponent,
-            ];
-          })}
-        <AddSection
-          setPageData={
-            setPageData as React.Dispatch<React.SetStateAction<AnyPageData>>
-          }
-          sections={availableSectionTypes}
-          page={"HOME"}
-          order={pageData.sections.length + 1}
+        <DashboardSections
+          userData={userData}
+          setUserData={setUserData}
+          pageData={pageData}
+          setPageData={setPageData}
         />
       </animated.div>
       <Footer />
