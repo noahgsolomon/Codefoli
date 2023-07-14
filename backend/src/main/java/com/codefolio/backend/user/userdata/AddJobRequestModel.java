40afb3ca9743e07546825100e57e4190e8078275
noah s <noahsolomon2003@gmail.com>
2023-07-14 17:15:00 -0400

    job response  and request model for adding a job

---
+++ backend/src/main/java/com/codefolio/backend/user/userdata/AddJobRequestModel.java
@@ -0,0 +1,4 @@
+package com.codefolio.backend.user.userdata;
+
+public record AddJobRequestModel(String company, String position, String description, String startDate, String endDate) {
+}
---
+++ backend/src/main/java/com/codefolio/backend/user/userdata/AddJobResponseModel.java
@@ -0,0 +1,4 @@
+package com.codefolio.backend.user.userdata;
+
+public record AddJobResponseModel(long id, String company , String position , String description , String startDate , String endDate) {
+}
