1328c802da0caee1ad00cc4816d8796205aa63e8
noah s <noahsolomon2003@gmail.com>
2023-07-19 08:00:12 -0400

    images for values

--- frontend/src/common/Type/Values.tsx
+++ frontend/src/common/Type/Values.tsx
@@ -63,12 +63,12 @@ export const ValuesData: {
   INTEGRITY: {
     description:
       "I believe in honesty and integrity in all my actions. I keep my promises and take responsibility for my actions.",
-    image: "https://img.icons8.com/cotton/400/ethics.png",
+    image: "https://img.icons8.com/cotton/400/field--v1.png",
   },
   QUALITY: {
     description:
       "I strive for excellence in everything I do. I am committed to delivering high-quality services to my clients.",
-    image: "https://img.icons8.com/cotton/400/quality.png",
+    image: "https://img.icons8.com/cotton/400/birthday-cake.png",
   },
   GROWTH: {
     description:
@@ -93,107 +93,107 @@ export const ValuesData: {
   TEAMWORK: {
     description:
       "I believe in teamwork and collaboration. I work well with others and value the contributions of my team members.",
-    image: "https://img.icons8.com/cotton/400/teamwork.png",
+    image: "https://img.icons8.com/cotton/400/leader-1.png",
   },
   RESPECT: {
     description:
       "I believe in respect for others. I treat everyone with dignity and respect, regardless of their position or background.",
-    image: "https://img.icons8.com/cotton/400/respect.png",
+    image: "https://img.icons8.com/cotton/400/trust--v1.png",
   },
   TRUST: {
     description:
       "I believe in trust and loyalty. I am trustworthy and loyal to my clients and colleagues.",
-    image: "https://img.icons8.com/cotton/400/trust.png",
+    image: "https://img.icons8.com/cotton/400/trust--v1.png",
   },
   ACCOUNTABILITY: {
     description:
       "I believe in accountability and responsibility. I take ownership of my actions and accept responsibility for my mistakes.",
-    image: "https://img.icons8.com/cotton/400/accountability.png",
+    image: "https://img.icons8.com/cotton/400/graph--v1.png",
   },
   PASSION: {
     description:
       "I believe in passion and enthusiasm. I am passionate about my work and enthusiastic about my clients’ success.",
-    image: "https://img.icons8.com/cotton/400/passion.png",
+    image: "https://img.icons8.com/cotton/400/fruit-ice-cream-cone.png",
   },
   FUN: {
     description:
       "I believe in having fun at work. I enjoy what I do and have fun doing it.",
-    image: "https://img.icons8.com/cotton/400/fun.png",
+    image: "https://img.icons8.com/cotton/400/slightly-smiling-face-icon.png",
   },
   CUSTOMER_SATISFACTION: {
     description:
       "I believe in customer satisfaction. I am committed to providing excellent service to my clients.",
-    image: "https://img.icons8.com/cotton/400/customer-satisfaction.png",
+    image: "https://img.icons8.com/cotton/400/completed-task--v2.png",
   },
   SOCIAL_RESPONSIBILITY: {
     description:
       "I believe in social responsibility. I am committed to making a positive impact on society and the environment.",
-    image: "https://img.icons8.com/cotton/400/social-responsibility.png",
+    image: "https://img.icons8.com/cotton/400/school-backpack--v1.png",
   },
   DIVERSITY: {
     description:
       "I believe in diversity and inclusion. I value the contributions of people from different backgrounds and cultures.",
-    image: "https://img.icons8.com/cotton/400/diversity.png",
+    image: "https://img.icons8.com/cotton/400/holy-bible.png",
   },
   EMPOWERMENT: {
     description:
       "I believe in empowerment and autonomy. I trust my team members to make decisions and take action without my approval.",
-    image: "https://img.icons8.com/cotton/400/empowerment.png",
+    image: "https://img.icons8.com/cotton/400/flex-biceps.png",
   },
   EXCELLENCE: {
     description:
       "I believe in excellence and quality. I strive for excellence in everything I do.",
-    image: "https://img.icons8.com/cotton/400/excellence.png",
+    image: "https://img.icons8.com/cotton/400/apple--v1.png",
   },
   CREATIVITY: {
     description:
       "I believe in creativity and innovation. I am always looking for new and better ways to solve problems.",
-    image: "https://img.icons8.com/cotton/400/creativity.png",
+    image: "https://img.icons8.com/cotton/400/paint-brush.png",
   },
   EFFICIENCY: {
     description:
       "I believe in efficiency and effectiveness. I am always looking for ways to improve my productivity and efficiency.",
-    image: "https://img.icons8.com/cotton/400/efficiency.png",
+    image: "https://img.icons8.com/cotton/400/stopwatch.png",
   },
   FLEXIBILITY: {
     description:
       "I believe in flexibility and adaptability. I am willing to change my plans when necessary.",
-    image: "https://img.icons8.com/cotton/400/flexibility.png",
+    image: "https://img.icons8.com/cotton/400/tape-measure.png",
   },
   HONESTY: {
     description:
       "I believe in honesty and integrity. I am always honest and upfront with my clients and colleagues.",
-    image: "https://img.icons8.com/cotton/400/honesty.png",
+    image: "https://img.icons8.com/cotton/400/speaker.png",
   },
   LOYALTY: {
     description:
       "I believe in loyalty and commitment. I am loyal to my clients and colleagues.",
-    image: "https://img.icons8.com/cotton/400/loyalty.png",
+    image: "https://img.icons8.com/cotton/400/dog--v3.png",
   },
   OPENNESS: {
     description:
       "I believe in openness and transparency. I am always honest and upfront with my clients and colleagues.",
-    image: "https://img.icons8.com/cotton/400/openness.png",
+    image: "https://img.icons8.com/cotton/400/open-box--v1.png",
   },
   RELIABILITY: {
     description:
       "I believe in reliability and dependability. I am always on time and do what I say I will do.",
-    image: "https://img.icons8.com/cotton/400/reliability.png",
+    image: "https://img.icons8.com/cotton/400/certificate--v1.png",
   },
   RESPONSIVENESS: {
     description:
       "I believe in responsiveness and availability. I am always available to my clients and colleagues.",
-    image: "https://img.icons8.com/cotton/400/responsiveness.png",
+    image: "https://img.icons8.com/cotton/400/conflict--v1.png",
   },
   SUSTAINABILITY: {
     description:
       "I believe in sustainability and environmental responsibility. I am committed to reducing my environmental impact.",
-    image: "https://img.icons8.com/cotton/400/sustainability.png",
+    image: "https://img.icons8.com/cotton/400/tree.png",
   },
   TRUSTWORTHINESS: {
     description:
       "I believe in trustworthiness and reliability. I am always on time and do what I say I will do.",
-    image: "https://img.icons8.com/cotton/400/trustworthiness.png",
+    image: "https://img.icons8.com/cotton/400/handshake--v2.png",
   },
 };
 
