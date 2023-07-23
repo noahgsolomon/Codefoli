eed30e311c92183d0f1234e9faf993f7c4ad1a4b
noah s <noahsolomon2003@gmail.com>
2023-07-23 19:38:26 -0400

    projects page updateable header and dashboard and fixed constantly recoloring for language.

--- frontend/src/pages/Projects/Projects.tsx
+++ frontend/src/pages/Projects/Projects.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, FC, SetStateAction } from "react";
+import { Dispatch, FC, SetStateAction, useMemo, useRef, useState } from "react";
 import ServiceCard from "Components/Sections/Skill/ServiceCard.tsx";
 import Footer from "Components/Footer/Footer";
 import ArrowRight from "assets/icons/arrow-right.svg";
@@ -6,33 +6,133 @@ import { COLORS } from "../../util/colors.ts";
 import UserData from "Type/UserData.tsx";
 import { Link } from "react-router-dom";
 import ProjectsPageData from "Type/ProjectsPageData.tsx";
+import {
+  updateDescriptionOneProjects,
+  updateHeaderOneProjects,
+} from "./projectspageapi.tsx";
 
 const Projects: FC<{
   userData: UserData;
   pageData: ProjectsPageData;
   setPageData: Dispatch<SetStateAction<ProjectsPageData>>;
-}> = ({ userData, pageData }) => {
+}> = ({ userData, pageData, setPageData }) => {
+  const [headerOneEdit, setHeaderOneEdit] = useState(false);
+  const [headerOneEditValue, setHeaderOneEditValue] = useState(
+    pageData.headerOne
+  );
+  const headerOneTextareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const [descriptionOneEdit, setDescriptionOneEdit] = useState(false);
+  const [descriptionOneEditValue, setDescriptionOneEditValue] = useState(
+    pageData.descriptionOne
+  );
+  const descriptionOneTextareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const projectColors = useMemo(() => {
+    let availableColors = [...COLORS];
+    return userData.projects.map(() => {
+      if (availableColors.length === 0) {
+        availableColors = [...COLORS];
+      }
+      const randomIndex = Math.floor(Math.random() * availableColors.length);
+      const selectedColor = availableColors[randomIndex];
+      availableColors.splice(randomIndex, 1);
+      return selectedColor;
+    });
+  }, [userData.projects]);
+  const handleHeaderOneSubmit = async () => {
+    const updateHeader = await updateHeaderOneProjects(headerOneEditValue);
+    if (updateHeader) {
+      setPageData((prev) => ({ ...prev, headerOne: headerOneEditValue }));
+      setHeaderOneEditValue(updateHeader.data);
+    }
+    setHeaderOneEdit(false);
+  };
+
+  const handleDescriptionOneSubmit = async () => {
+    const updateDescription = await updateDescriptionOneProjects(
+      descriptionOneEditValue
+    );
+    if (updateDescription) {
+      setPageData((prev) => ({
+        ...prev,
+        descriptionOne: descriptionOneEditValue,
+      }));
+      setDescriptionOneEditValue(updateDescription);
+    }
+    setDescriptionOneEdit(false);
+  };
+
   return (
     <>
       <main>
         <div className="container mx-auto my-20 max-w-screen-lg px-5">
           <section>
-            <h1 className="mb-5 text-center text-3xl font-bold md:text-5xl lg:text-6xl">
-              {pageData.headerOne}
-            </h1>
-            <p className="text-center font-semibold">
-              {pageData.descriptionOne}
-            </p>
+            {headerOneEdit ? (
+              <textarea
+                ref={headerOneTextareaRef}
+                value={headerOneEditValue}
+                onChange={(e) => setHeaderOneEditValue(e.target.value)}
+                onBlur={() => {
+                  setHeaderOneEditValue(pageData.headerOne);
+                  setHeaderOneEdit(false);
+                }}
+                onKeyDown={async (e) => {
+                  if (e.key === "Enter") {
+                    e.preventDefault();
+                    await handleHeaderOneSubmit();
+                  }
+                }}
+                className="mb-5 w-full resize-none appearance-none overflow-hidden border-none bg-transparent text-center text-3xl font-bold leading-snug outline-none focus:outline-none focus:ring-0 md:text-5xl lg:text-6xl"
+                autoFocus
+                onFocus={(e) => {
+                  e.target.select();
+                }}
+                maxLength={25}
+              />
+            ) : (
+              <h1
+                className="mb-5 cursor-pointer select-none text-center text-3xl font-bold leading-snug transition-all hover:opacity-50 md:text-5xl lg:text-6xl"
+                onClick={() => setHeaderOneEdit(true)}
+              >
+                {pageData.headerOne}
+              </h1>
+            )}
+            {descriptionOneEdit ? (
+              <textarea
+                ref={descriptionOneTextareaRef}
+                value={descriptionOneEditValue}
+                onChange={(e) => setDescriptionOneEditValue(e.target.value)}
+                onBlur={() => {
+                  setDescriptionOneEditValue(pageData.descriptionOne);
+                  setDescriptionOneEdit(false);
+                }}
+                onKeyDown={async (e) => {
+                  if (e.key === "Enter") {
+                    e.preventDefault();
+                    await handleDescriptionOneSubmit();
+                  }
+                }}
+                className="w-full resize-none appearance-none overflow-hidden border-none bg-transparent text-center text-lg leading-snug outline-none focus:outline-none focus:ring-0"
+                autoFocus
+                onFocus={(e) => e.currentTarget.select()}
+                maxLength={250}
+                rows={3}
+              />
+            ) : (
+              <p
+                className="cursor-pointer select-none text-center font-semibold transition-all hover:opacity-50"
+                onClick={() => setDescriptionOneEdit(true)}
+              >
+                {pageData.descriptionOne}
+              </p>
+            )}
           </section>
         </div>
       </main>
-      {/* projects */}
       <section>
         <div className="container mx-auto mb-5 px-5">
-          {/* projects */}
           <div className="projects grid grid-cols-1 justify-items-center gap-5 md:grid-cols-2 lg:grid-cols-3">
             {userData.projects.map(
-              ({ name, description, language, imageUrl = "" }) => {
+              ({ name, description, language, imageUrl = "" }, index) => {
                 return (
                   <ServiceCard
                     title={name}
@@ -53,9 +153,7 @@ const Projects: FC<{
                     </Link>
                     <div className={`rounded-b-lg bg-white px-5 py-2`}>
                       <span
-                        className={`mb-2 mr-2 inline-block cursor-pointer rounded-lg px-3 text-white transition-all hover:-translate-y-0.5 ${
-                          COLORS[Math.floor(Math.random() * COLORS.length)]
-                        } py-2 text-sm`}
+                        className={`mb-2 mr-2 inline-block cursor-pointer rounded-lg px-3 text-white transition-all hover:-translate-y-0.5 ${projectColors[index]} py-2 text-sm`}
                       >
                         {language}
                       </span>
