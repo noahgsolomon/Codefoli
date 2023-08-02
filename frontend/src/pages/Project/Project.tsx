2286f9d4a7989fa20282b5e45fc519db18a9f588
noah s <noahsolomon2003@gmail.com>
2023-08-02 10:40:29 -0400

    not allowing for duplicate languages, and added header update functionality.

--- frontend/src/pages/Project/Project.tsx
+++ frontend/src/pages/Project/Project.tsx
@@ -1,9 +1,14 @@
 import LinkIcon from "assets/icons/arrow-up-right.svg";
 import Footer from "Components/Footer/Footer";
 import { COLORS } from "../../util/colors.ts";
-import { Dispatch, FC, SetStateAction, useState } from "react";
+import { Dispatch, FC, SetStateAction, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import UserData from "Type/UserData.tsx";
+import { updateProjectHeader } from "./projectapi.tsx";
+import {
+  addProjectLanguage,
+  removeProjectLanguage,
+} from "../Projects/projectspageapi.tsx";
 
 const Project: FC<{
   userData: UserData;
@@ -19,20 +24,124 @@ const Project: FC<{
   const [projectData, setProjectData] = useState(
     userData.projects.find((p) => p.slug === slug)
   );
+  const [headerEdit, setHeaderEdit] = useState(false);
+  const [headerEditValue, setHeaderEditValue] = useState(
+    projectDetails?.header || ""
+  );
+  const headerTextareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const [languageHover, setLanguageHover] = useState(-1);
+  const [addProjectLanguageState, setAddProjectLanguageState] = useState(false);
+  const [languageAddValue, setLanguageAddValue] = useState("");
 
-  if (!projectDetails) {
+  if (!projectDetails || !slug || !projectData) {
     navigate("/404");
     return null;
   }
+
+  const handleHeaderSubmit = async () => {
+    const updateHeader = await updateProjectHeader(slug, headerEditValue);
+    if (updateHeader) {
+      const updatedSlugs = userData.slugs.map((s) =>
+        s.slug === slug ? { ...s, header: headerEditValue } : s
+      );
+
+      setUserData((prev) => ({
+        ...prev,
+        slugs: updatedSlugs,
+      }));
+
+      setProjectDetails(updatedSlugs.find((s) => s.slug === slug));
+      setHeaderEditValue(updateHeader.data);
+    }
+    setHeaderEdit(false);
+  };
+
+  const handleAddLanguage = async (language: string) => {
+    if (
+      projectData.languages.some(
+        (lang) => language.toLowerCase() === lang.toLowerCase()
+      )
+    ) {
+      setLanguageAddValue("");
+      setAddProjectLanguageState(false);
+      return;
+    }
+    const updateLanguages = await addProjectLanguage(projectData.id, language);
+    if (updateLanguages) {
+      const updatedProjects = userData.projects.map((p) =>
+        p.slug === slug
+          ? { ...p, languages: [...p.languages, updateLanguages.data] }
+          : p
+      );
+
+      setUserData((prev) => ({
+        ...prev,
+        projects: updatedProjects,
+      }));
+
+      setProjectData(updatedProjects.find((p) => p.slug === slug));
+      setLanguageAddValue("");
+      setAddProjectLanguageState(false);
+    }
+  };
+
+  const handleRemoveLanguage = async (language: string) => {
+    const updateLanguages = await removeProjectLanguage(
+      projectData.id,
+      language
+    );
+    if (updateLanguages.status === "OK") {
+      const updatedProjects = userData.projects.map((p) =>
+        p.slug === slug
+          ? { ...p, languages: p.languages.filter((l) => l !== language) }
+          : p
+      );
+
+      setUserData((prev) => ({
+        ...prev,
+        projects: updatedProjects,
+      }));
+      setProjectData(updatedProjects.find((p) => p.slug === slug));
+    }
+  };
 
   return (
     <>
       <main>
         <div className="container mx-auto my-20 max-w-screen-lg px-5">
           <section className="hero mb-8">
-            <h1 className="mb-5 text-center text-4xl font-bold">
-              {projectDetails.header}
-            </h1>
+            {headerEdit ? (
+              <textarea
+                ref={headerTextareaRef}
+                value={headerEditValue}
+                onChange={(e) => setHeaderEditValue(e.target.value)}
+                onBlur={() => {
+                  setHeaderEditValue(projectDetails.header);
+                  setHeaderEdit(false);
+                }}
+                onKeyDown={async (e) => {
+                  if (e.key === "Enter") {
+                    e.preventDefault();
+                    await handleHeaderSubmit();
+                  }
+                }}
+                className="w-full resize-none appearance-none overflow-hidden border-none bg-transparent text-center text-4xl font-bold leading-snug outline-none focus:outline-none focus:ring-0"
+                autoFocus
+                onFocus={(e) => {
+                  e.target.select();
+                }}
+                maxLength={25}
+              />
+            ) : (
+              <h1
+                className="mb-5 cursor-pointer select-none text-center text-4xl font-bold leading-snug transition-all hover:opacity-50"
+                onClick={() => {
+                  setHeaderEdit(true);
+                }}
+              >
+                {projectDetails.header}
+              </h1>
+            )}
             <p className="mb-5 text-center font-semibold">
               {projectDetails.about}
             </p>
@@ -58,18 +167,54 @@ const Project: FC<{
                 </div>
                 <div className="li-wrapper">
                   <li className="font-bold">Tech Stack</li>
-                  {projectData?.languages.map((tech, index) => {
-                    return (
+                  {projectData &&
+                    projectData.languages.map((language, index) => (
                       <span
-                        key={index}
                         className={`mb-2 mr-2 inline-block cursor-pointer rounded-lg px-3 text-white transition-all hover:-translate-y-0.5 ${
-                          COLORS[Math.floor(Math.random() * COLORS.length)]
+                          languageHover === index
+                            ? "bg-red-500 line-through"
+                            : COLORS[index]
                         } py-2 text-sm`}
+                        onMouseEnter={() => setLanguageHover(index)}
+                        onMouseLeave={() => setLanguageHover(-1)}
+                        onClick={async () => handleRemoveLanguage(language)}
+                        key={index}
                       >
-                        {tech}
+                        {language}
                       </span>
-                    );
-                  })}
+                    ))}
+                  {projectData &&
+                    projectData.languages.length < 8 &&
+                    !addProjectLanguageState && (
+                      <div
+                        className={`mb-2 mr-2 inline-block cursor-pointer rounded-lg bg-gray-300 px-3 py-2 text-sm text-white transition-all hover:-translate-y-0.5`}
+                        onClick={() => setAddProjectLanguageState(true)}
+                      >
+                        +
+                      </div>
+                    )}
+                  {addProjectLanguageState && (
+                    <input
+                      type="text"
+                      className="mb-2 mr-2 inline-block cursor-pointer rounded-lg px-3 py-2 text-sm shadow-custom transition-all hover:-translate-y-0.5 focus:outline-none"
+                      value={languageAddValue}
+                      onChange={(e) => setLanguageAddValue(e.target.value)}
+                      onBlur={() => {
+                        setAddProjectLanguageState(false);
+                        setLanguageAddValue("");
+                      }}
+                      autoFocus
+                      onFocus={(e) => e.currentTarget.select()}
+                      maxLength={25}
+                      onKeyDown={async (e) => {
+                        if (e.key === "Enter") {
+                          e.preventDefault();
+                          await handleAddLanguage(languageAddValue);
+                          setLanguageAddValue("");
+                        }
+                      }}
+                    />
+                  )}
                 </div>
               </ul>
               <a
