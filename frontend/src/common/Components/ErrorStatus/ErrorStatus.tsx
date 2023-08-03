f99275eb6ca916711e50e4229999109f83534a40
noah s <noahsolomon2003@gmail.com>
2023-08-03 12:51:06 -0400

    error status component

---
+++ frontend/src/common/Components/ErrorStatus/ErrorStatus.tsx
@@ -0,0 +1,50 @@
+import { FC, useEffect, useState } from "react";
+import { useSpring, animated } from "react-spring";
+
+const ErrorStatus: FC<{
+  message: string;
+}> = ({ message }) => {
+  const [show, setShow] = useState(false);
+  const [animateDown, setAnimateDown] = useState(false);
+  const [animateUp, setAnimateUp] = useState(false);
+
+  const animate = useSpring({
+    opacity: show ? 1 : 0,
+    top: animateUp ? "-100px" : animateDown ? "50px" : "-100px",
+  });
+
+  useEffect(() => {
+    setShow(true);
+    setAnimateDown(true);
+    const timer = setTimeout(() => {
+      setAnimateUp(true);
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (animateUp) {
+      const timer = setTimeout(() => {
+        setShow(false);
+      }, 1000);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [animateUp]);
+
+  return (
+    <animated.div
+      style={animate}
+      className={
+        "fixed left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 transform rounded-xl border-2 border-black bg-red-400 px-3 text-lg text-white md:px-6 md:py-3 md:text-2xl md:font-bold"
+      }
+    >
+      {message}
+    </animated.div>
+  );
+};
+
+export default ErrorStatus;
