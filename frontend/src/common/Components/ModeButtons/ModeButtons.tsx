9721e597fb6f7d5c18dbf1e67eeceec97727d796
noah s <noahsolomon2003@gmail.com>
2023-08-06 16:13:53 -0400

    modebuttons configured for handling deployment

--- frontend/src/common/Components/ModeButtons/ModeButtons.tsx
+++ frontend/src/common/Components/ModeButtons/ModeButtons.tsx
@@ -1,8 +1,15 @@
 import { useState, useEffect, FC } from "react";
 import { useSpring, animated } from "react-spring";
-import {deploy} from "api/deployapi.tsx";
+import { deploy } from "api/deployapi.tsx";
+import UserData from "Type/UserData.tsx";
 
-const ModeButtons: FC = () => {
+const ModeButtons: FC<{
+  deploying: boolean;
+  setDeploying: (deploying: boolean) => void;
+  setDeployed: (deployed: { url: string; bool: boolean }) => void;
+  userData: UserData;
+  setUserData: (userData: UserData) => void;
+}> = ({ deploying, setDeployed, setDeploying, userData, setUserData }) => {
   const [scrollingDown, setScrollingDown] = useState(false);
   const [prevScroll, setPrevScroll] = useState(window.scrollY);
   const thresholdShow = 200;
@@ -40,6 +47,16 @@ const ModeButtons: FC = () => {
     config: { duration: 100 },
   });
 
+  const handleDeploy = async () => {
+    setDeploying(true);
+    const deployFetch = await deploy();
+    if (deployFetch.status === "OK") {
+      setDeployed({ url: deployFetch.data, bool: true });
+      setDeploying(false);
+      setUserData({ ...userData, website: deployFetch.data });
+    }
+  };
+
   return (
     <animated.div
       style={animation}
@@ -50,16 +67,40 @@ const ModeButtons: FC = () => {
           "flex flex-row rounded-3xl border-2 border-black bg-white px-4 py-3 shadow-custom"
         }
       >
-        <a href="/preview">
-          <button className="mr-2 flex h-12 w-40 justify-center rounded-3xl border-2 border-black bg-green-400 text-white transition-all hover:-translate-y-0.5 hover:shadow-custom">
-            Preview ↗
-          </button>
-        </a>
-        <button className="flex h-12 w-40 justify-center rounded-3xl border-2 border-black bg-blue-500 text-white transition-all hover:-translate-y-0.5 hover:shadow-custom"
-        onClick={async () => await deploy()}
-        >
-          Deploy <span className={"text-2xl text-white"}>&nbsp; ✈</span>
-        </button>
+        {!deploying ? (
+          <>
+            <a href="/preview">
+              <button className="mr-2 flex h-12 w-40 justify-center rounded-3xl border-2 border-black bg-green-400 text-white transition-all hover:-translate-y-0.5 hover:shadow-custom">
+                Preview ↗
+              </button>
+            </a>
+            <button
+              className="flex h-12 w-40 justify-center rounded-3xl border-2 border-black bg-blue-500 text-white transition-all hover:-translate-y-0.5 hover:shadow-custom"
+              onClick={async () => await handleDeploy()}
+            >
+              Deploy <span className={"text-2xl text-white"}>&nbsp; ✈</span>
+            </button>
+          </>
+        ) : (
+          <div className="flex h-12 w-40 items-center justify-center rounded-3xl border-2 border-black bg-blue-500 text-white transition-all hover:-translate-y-0.5 hover:shadow-custom">
+            <svg className="mr-2 h-5 w-5 animate-spin" viewBox="0 0 24 24">
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Deploying...
+          </div>
+        )}
       </div>
     </animated.div>
   );
--- frontend/src/preview/common/Components/ModeButtons/ModeButtonsP.tsx
+++ frontend/src/preview/common/Components/ModeButtons/ModeButtonsP.tsx
@@ -1,7 +1,16 @@
 import { useState, useEffect, FC } from "react";
 import { useSpring, animated } from "react-spring";
+import { deploy } from "api/deployapi.tsx";
+import UserData from "Type/UserData.tsx";
+import userData from "Type/UserData.tsx";
 
-const ModeButtonsP: FC = () => {
+const ModeButtonsP: FC<{
+  deploying: boolean;
+  setDeploying: (deploying: boolean) => void;
+  setDeployed: (deployed: { url: string; bool: boolean }) => void;
+  userData: userData;
+  setUserData: (userData: UserData) => void;
+}> = ({ deploying, setDeploying, setDeployed, userData, setUserData }) => {
   const [scrollingDown, setScrollingDown] = useState(false);
   const [prevScroll, setPrevScroll] = useState(window.scrollY);
   const thresholdShow = 200;
@@ -39,6 +48,16 @@ const ModeButtonsP: FC = () => {
     config: { duration: 100 },
   });
 
+  const handleDeploy = async () => {
+    setDeploying(true);
+    const deployFetch = await deploy();
+    if (deployFetch.status === "OK") {
+      setDeployed({ url: deployFetch.data, bool: true });
+      setDeploying(false);
+      setUserData({ ...userData, website: deployFetch.data });
+    }
+  };
+
   return (
     <animated.div
       style={animation}
@@ -49,14 +68,53 @@ const ModeButtonsP: FC = () => {
           "flex flex-row rounded-3xl border-2 border-black bg-white px-4 py-3 shadow-custom"
         }
       >
-        <a href={"/dashboard"}>
-          <button className="mr-2 flex h-12 w-40 justify-center rounded-3xl border-2 border-black bg-black text-white transition-all hover:-translate-y-0.5 hover:shadow-custom">
-            Edit ↗
-          </button>
-        </a>
-        <button className="flex h-12 w-40 justify-center rounded-3xl border-2 border-black bg-blue-500 text-white transition-all hover:-translate-y-0.5 hover:shadow-custom">
-          Deploy <span className={"text-2xl text-white"}>&nbsp; ✈</span>
-        </button>
+        {!deploying ? (
+          <div className={"flex flex-col items-center justify-center"}>
+            <div className={"flex flex-row"}>
+              <a href={"/dashboard"}>
+                <button className="mr-2 flex h-12 w-40 justify-center rounded-3xl border-2 border-black bg-black text-white transition-all hover:-translate-y-0.5 hover:shadow-custom">
+                  Edit ↗
+                </button>
+              </a>
+              <button
+                className="flex h-12 w-40 justify-center rounded-3xl border-2 border-black bg-blue-500 text-white transition-all hover:-translate-y-0.5 hover:shadow-custom"
+                onClick={async () => await handleDeploy()}
+              >
+                Deploy <span className={"text-2xl text-white"}>&nbsp; ✈</span>
+              </button>
+            </div>
+            <div>
+              <a
+                href={userData.website}
+                className={
+                  "break-all text-sm text-blue-500 underline transition-all hover:text-yellow-500"
+                }
+                target={"_blank"}
+              >
+                {userData.website}
+              </a>
+            </div>
+          </div>
+        ) : (
+          <div className="flex h-12 w-40 items-center justify-center rounded-3xl border-2 border-black bg-blue-500 text-white transition-all hover:-translate-y-0.5 hover:shadow-custom">
+            <svg className="mr-2 h-5 w-5 animate-spin" viewBox="0 0 24 24">
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Deploying...
+          </div>
+        )}
       </div>
     </animated.div>
   );
