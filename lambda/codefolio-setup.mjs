fdef6260d3a26a1de2b5c42ba21f2a5abe2c791e
noah s <noahsolomon2003@gmail.com>
2023-08-08 11:11:17 -0400

    start of lambda setup function. It right now grabs the username id value from cognito, uses that to find the user in the users table in the database, check if the role is newbie, if so, update the company, about, role, profession, and location for that user.

---
+++ lambda/codefolio-setup.mjs
@@ -0,0 +1,67 @@
+import Knex from 'knex';
+import jwt from 'jsonwebtoken';
+
+const handler = async (event) => {
+    console.log("Received event:", JSON.stringify(event));
+
+    const idToken = event.headers.Authorization.replace('Bearer ', '');
+    const decodedToken = jwt.decode(idToken);
+    const cognitoUserId = decodedToken['sub'];
+
+    const user = '****';
+    const password = '****';
+    const host = '****';
+    const database = 'codefolio';
+
+    const connection = {
+        ssl: { rejectUnauthorized: false },
+        host,
+        user,
+        password,
+        database
+    };
+    // Connect to the RDS database
+    console.log("Attempting to connect to the database...");
+    const knex = Knex({
+        client: 'postgres',
+        connection
+    });
+    console.log("Connected to the database.");
+
+    const existingUser = await knex('users')
+        .where('cognito_user_id', cognitoUserId)
+        .first();
+
+    if (existingUser.role !== 'NEWBIE') {
+        console.error("User role is not 'NEWBIE'");
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: "User role is not 'NEWBIE'" }),
+        };
+    }
+
+    const body = JSON.parse(event.body);
+
+    await knex('users')
+        .where('cognito_user_id', cognitoUserId)
+        .update({
+            company: body.company,
+            location: body.location,
+            about: body.about,
+            profession: body.profession,
+            role: 'USER'
+        });
+    console.log("User details updated successfully");
+
+    const updatedUser = await knex('users')
+        .where('cognito_user_id', cognitoUserId)
+        .first();
+    console.log("Updated user details:", updatedUser);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(updatedUser),
+    };
+};
+
+export { handler };
