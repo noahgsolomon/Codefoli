893ff2b8aab56ad188f7ab1a6ee2060b750b882b
noah s <noahsolomon2003@gmail.com>
2023-08-10 18:09:51 -0400

    login returns standardized + w/ refresh token

--- lambda/email-login.mjs
+++ lambda/email-login.mjs
@@ -5,7 +5,7 @@ const client = new CognitoIdentityProviderClient({ region: "us-east-1" });
 const handler = async (event) => {
     const command = new InitiateAuthCommand({
         AuthFlow: "USER_PASSWORD_AUTH",
-        ClientId: "****",
+        ClientId: "5et40k0da1hoosqmjf5peo0i8j",
         AuthParameters: {
             USERNAME: event.username,
             PASSWORD: event.password
@@ -17,14 +17,23 @@ const handler = async (event) => {
         return {
             statusCode: 200,
             body: JSON.stringify({
-                idToken: response.AuthenticationResult.IdToken,
-                accessToken: response.AuthenticationResult.AccessToken
+                status: "OK",
+                message: "User logged in successfully",
+                data: {
+                    idToken: response.AuthenticationResult.IdToken,
+                    accessToken: response.AuthenticationResult.AccessToken,
+                    refreshToken: response.AuthenticationResult.RefreshToken // Include the refresh token
+                }
             }),
         };
     } catch (err) {
         return {
             statusCode: 400,
-            body: JSON.stringify(err),
+            body: JSON.stringify({
+                status: "ERROR",
+                message: err.message,
+                data: null
+            }),
         };
     }
 };
