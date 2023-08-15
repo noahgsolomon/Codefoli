ef6ee1d2b2bd3944d5eb0eb41493bdd0528be187
noah s <noahsolomon2003@gmail.com>
2023-08-15 15:28:19 -0400

    changed loader style

--- frontend/src/common/Components/Loader/Loader.tsx
+++ frontend/src/common/Components/Loader/Loader.tsx
@@ -1,33 +1,27 @@
-import React from "react";
+import {FC} from "react";
 
-type Props = {
-  message?: string;
-};
-
-const Loader: React.FC<Props> = ({ message }) => {
+const Loader: FC = () => {
   return (
     <div className="fixed left-0 top-0 z-30 h-screen w-screen bg-gray-50">
       <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center">
         <div role="status">
-          <svg
-            aria-hidden="true"
-            className="mr-2 h-8 w-8 animate-spin text-gray-200"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+          <svg className="mr-2 h-10 w-10 animate-spin" viewBox="0 0 24 24">
+            <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+            ></circle>
             <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="#B2BEB5"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="#007BFF"
-            />
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
           </svg>
-          <span className="sr-only">Loading...</span>
         </div>
-        {message && <p className="text-center">{message}</p>}
+        <span className="text-center font-bold">Loading</span>
       </div>
     </div>
   );
