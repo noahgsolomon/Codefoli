98eeadad74eaa90adf18623356594935c7e6d044
noah s <noahsolomon2003@gmail.com>
2023-08-15 21:08:49 -0400

    changed loader style

--- frontend/src/common/Components/Loader/Loader.tsx
+++ frontend/src/common/Components/Loader/Loader.tsx
@@ -5,19 +5,19 @@ const Loader: FC = () => {
     <div className="fixed left-0 top-0 z-30 h-screen w-screen bg-gray-50">
       <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center">
         <div role="status">
-          <svg className="mr-2 h-10 w-10 animate-spin" viewBox="0 0 24 24">
+          <svg className="mr-2 border-2 border-gray-200 rounded-full h-10 w-10 animate-spin" viewBox="0 0 24 24">
             <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
+                className="rainbow-stroke"
+                cx="12"
+                cy="12"
+                r="10"
+                strokeWidth="4"
+                fill={'white'}
             ></circle>
             <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                className="opacity-75"
+                fill="white"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
             ></path>
           </svg>
         </div>
