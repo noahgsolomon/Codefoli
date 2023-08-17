5e38676451e55ed39559cd8d4160930e41feb4a6
noah s <noahsolomon2003@gmail.com>
2023-08-17 16:50:14 -0400

    deploy configured logic. The architecture was also created. The api gateway endpoint invokes a lambda function which confirms the user and relays the message after standardizing it to the sqs queue, which triggers a processing lambda function which reads the message, creates the website and delete the msg

--- frontend/src/util/api/deployapi.tsx
+++ frontend/src/util/api/deployapi.tsx
@@ -1,12 +1,15 @@
 const deploy = async () => {
   try {
-    const response = await fetch("http://localhost:8080/deploy", {
+    const response = await fetch("https://f60z27ge89.execute-api.us-east-1.amazonaws.com/prod/deploy", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("Id")
+      },
     });
 
     const responseJson = await response.json();
+    console.log(responseJson)
     if (responseJson.status === "OK") {
       return responseJson;
     } else {
