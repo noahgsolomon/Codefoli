8ffbd41db2a557ac098095a797bffd09f908acdd
noah s <noahsolomon2003@gmail.com>
2023-08-17 16:52:57 -0400

    project api configured to integrate updateProjectHeader,updateProjectAbout,updateProjectOverview,updateProjectDescription, updateProjectPlatforms,updateProjectLink, all in one endpoint

--- frontend/src/pages/Project/projectapi.tsx
+++ frontend/src/pages/Project/projectapi.tsx
@@ -1,155 +1,20 @@
-const updateProjectHeader = async (slug: string, header: string) => {
-  const model = {
-    slug: slug,
-    content: header,
-  };
+
+
+const changeProject = async (
+    project:
+        ({text:string, id: string, type:'link' | 'platforms' | 'description' | 'overview' | 'about' | 'header'})) => {
   try {
     const response = await fetch(
-      `http://localhost:8080/project-content/header`,
-      {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(model),
-        credentials: "include",
-      }
+        "https://f60z27ge89.execute-api.us-east-1.amazonaws.com/prod/project",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage.getItem("Id"),
+          },
+          body: JSON.stringify(project),
+        }
     );
-
-    const responseJson = await response.json();
-    if (responseJson.status === "OK") {
-      return responseJson;
-    } else {
-      console.log(responseJson.message);
-      return responseJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const updateProjectAbout = async (slug: string, about: string) => {
-  const model = {
-    slug: slug,
-    content: about,
-  };
-  try {
-    const response = await fetch(
-      `http://localhost:8080/project-content/about`,
-      {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(model),
-        credentials: "include",
-      }
-    );
-
-    const responseJson = await response.json();
-    if (responseJson.status === "OK") {
-      return responseJson;
-    } else {
-      console.log(responseJson.message);
-      return responseJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const updateProjectOverview = async (slug: string, overview: string) => {
-  const model = {
-    slug: slug,
-    content: overview,
-  };
-  try {
-    const response = await fetch(
-      `http://localhost:8080/project-content/overview`,
-      {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(model),
-        credentials: "include",
-      }
-    );
-
-    const responseJson = await response.json();
-    if (responseJson.status === "OK") {
-      return responseJson;
-    } else {
-      console.log(responseJson.message);
-      return responseJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const updateProjectDescription = async (slug: string, description: string) => {
-  const model = {
-    slug: slug,
-    content: description,
-  };
-  try {
-    const response = await fetch(
-      `http://localhost:8080/project-content/description`,
-      {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(model),
-        credentials: "include",
-      }
-    );
-
-    const responseJson = await response.json();
-    if (responseJson.status === "OK") {
-      return responseJson;
-    } else {
-      console.log(responseJson.message);
-      return responseJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const updateProjectPlatforms = async (slug: string, platforms: string) => {
-  const model = {
-    slug: slug,
-    content: platforms,
-  };
-  try {
-    const response = await fetch(
-      `http://localhost:8080/project-content/platforms`,
-      {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(model),
-        credentials: "include",
-      }
-    );
-
-    const responseJson = await response.json();
-    if (responseJson.status === "OK") {
-      return responseJson;
-    } else {
-      console.log(responseJson.message);
-      return responseJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const updateProjectLink = async (slug: string, link: string) => {
-  const model = {
-    slug: slug,
-    content: link,
-  };
-  try {
-    const response = await fetch(`http://localhost:8080/project-content/link`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
 
     const responseJson = await response.json();
     if (responseJson.status === "OK") {
@@ -164,10 +29,5 @@ const updateProjectLink = async (slug: string, link: string) => {
 };
 
 export {
-  updateProjectHeader,
-  updateProjectAbout,
-  updateProjectOverview,
-  updateProjectDescription,
-  updateProjectPlatforms,
-  updateProjectLink,
+    changeProject,
 };
