e9866f6f16aadda856048e53fc22c8446fc09117
noah s <noahsolomon2003@gmail.com>
2023-08-16 20:27:53 -0400

    section api for addremove section configured

--- frontend/src/common/Components/Sections/api/sectionapi.tsx
+++ frontend/src/common/Components/Sections/api/sectionapi.tsx
@@ -1,35 +1,42 @@
 import PageType from "Type/Pages.tsx";
 import { SectionType } from "Type/Section.tsx";
 
-const removeSection = async (
+const addRemoveSection = async (
   page: PageType,
   section: SectionType,
-  order: number
+  page_order: number,
+  type: "add" | "remove"
 ) => {
   const model = {
     page: page,
     section: section,
-    order: order,
+    page_order: page_order,
+    type: type,
   };
   try {
-    const updateFetch = await fetch(`http://localhost:8080/remove-section`, {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
+    const updateFetch = await fetch(
+      `https://f60z27ge89.execute-api.us-east-1.amazonaws.com/prod/add-remove-section`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("Id"),
+        },
+        body: JSON.stringify(model),
+      }
+    );
 
     const updateFetchJson = await updateFetch.json();
-
+    console.log(updateFetchJson);
     if (updateFetchJson.status === "OK") {
-      return updateFetchJson.data;
+      return updateFetchJson;
     } else {
       console.log(updateFetchJson.message);
-      return updateFetchJson.message;
+      return updateFetchJson;
     }
   } catch (e) {
     console.log(e);
   }
 };
 
-export { removeSection };
+export { addRemoveSection };
