930b4c372b078911b8938f88d73efdcf1d419610
noah s <noahsolomon2003@gmail.com>
2023-08-17 16:54:40 -0400

    add value card configured to work with change value endpoint

--- frontend/src/common/Components/Sections/Value/AddValueCard.tsx
+++ frontend/src/common/Components/Sections/Value/AddValueCard.tsx
@@ -2,7 +2,7 @@ import { ChangeEvent, Dispatch, FC, SetStateAction, useState } from "react";
 import { Values, ValuesFormatted } from "Type/Values.tsx";
 import AnyPageData from "Type/AnyPageData.tsx";
 import { ValueType } from "Type/Section.tsx";
-import { addValue } from "Components/Sections/Value/valueapi.tsx";
+import {changeValue} from "Components/Sections/Value/valueapi.tsx";
 
 const AddJobCard: FC<{
   setPageData: Dispatch<SetStateAction<AnyPageData>>;
@@ -24,6 +24,37 @@ const AddJobCard: FC<{
     );
     setMatchingValues(matches);
   };
+
+  const handleAddValue = async (value: string) => {
+    const newValue = value.toUpperCase().replaceAll(" ", "_");
+    const addValueFetch = await changeValue({operation: 'add', value: newValue});
+    if (addValueFetch.status === "OK") {
+      setPageData((prev) => ({
+        ...prev,
+        sections: prev.sections.map((section) =>
+            section.type === "VALUE" &&
+            "values" in section.details
+                ? {
+                  ...section,
+                  details: {
+                    ...section.details,
+                    values: [
+                      ...section.details.values,
+                      {
+                        value:
+                            newValue as ValuesFormatted,
+                      },
+                    ],
+                  },
+                }
+                : section
+        ),
+      }));
+    }
+    setAddValueInput(false);
+    setNewValue("");
+    setMatchingValues([...allValues]);
+  }
 
   return (
     <div
@@ -52,36 +83,7 @@ const AddJobCard: FC<{
                   <div
                     key={value}
                     className="m-1 inline-block cursor-pointer rounded-full border-b border-gray-300 bg-black transition-all hover:-translate-y-0.5 hover:opacity-90"
-                    onClick={async () => {
-                      const newValue = value.toUpperCase().replaceAll(" ", "_");
-                      const addValueFetch = await addValue(newValue);
-                      if (addValueFetch.status === "OK") {
-                        setPageData((prev) => ({
-                          ...prev,
-                          sections: prev.sections.map((section) =>
-                            section.type === "VALUE" &&
-                            "values" in section.details
-                              ? {
-                                  ...section,
-                                  details: {
-                                    ...section.details,
-                                    values: [
-                                      ...section.details.values,
-                                      {
-                                        value:
-                                          addValueFetch.data as ValuesFormatted,
-                                      },
-                                    ],
-                                  },
-                                }
-                              : section
-                          ),
-                        }));
-                      }
-                      setAddValueInput(false);
-                      setNewValue("");
-                      setMatchingValues([...allValues]);
-                    }}
+                    onClick={async () => await handleAddValue(value)}
                   >
                     <span className="px-2 text-white">{value}</span>
                   </div>
