39ce3bf52b2c82256ba052710d158c8919f080f4
noah s <noahsolomon2003@gmail.com>
2023-08-17 16:50:59 -0400

    project page configured to integrate remove project, add project, update project title and description, and add and remove languages in a single endpoint

--- frontend/src/pages/Projects/projectspageapi.tsx
+++ frontend/src/pages/Projects/projectspageapi.tsx
@@ -1,145 +1,22 @@
-const updateProjectTitle = async (id: string, title: string) => {
-  const model = {
-    title: title,
-    id: id,
-  };
-  try {
-    const updateFetch = await fetch("http://localhost:8080/project/title", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
-    const updateFetchJson = await updateFetch.json();
-    if (updateFetchJson.status === "OK") {
-      return updateFetchJson;
-    } else {
-      console.log(updateFetchJson.message);
-      return updateFetchJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const updateProjectDescription = async (id: string, description: string) => {
-  const model = {
-    description: description,
-    id: id,
-  };
-  try {
-    const updateFetch = await fetch(
-      "http://localhost:8080/project/description",
-      {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(model),
-        credentials: "include",
-      }
-    );
-    const updateFetchJson = await updateFetch.json();
-    if (updateFetchJson.status === "OK") {
-      return updateFetchJson;
-    } else {
-      console.log(updateFetchJson.message);
-      return updateFetchJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const removeProject = async (id: string) => {
-  try {
-    const response = await fetch("http://localhost:8080/project/remove", {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-      body: id,
-      credentials: "include",
-    });
-
-    const responseJson = await response.json();
-    if (responseJson.status === "OK") {
-      return responseJson;
-    } else {
-      console.log(responseJson.message);
-      return responseJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const addProject = async (
-  title: string,
-  description: string,
-  language: string
-) => {
-  const model = {
-    title: title,
-    description: description,
-    language: language,
-  };
-  try {
-    const response = await fetch("http://localhost:8080/project/add", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
-
-    const responseJson = await response.json();
-    if (responseJson.status === "OK") {
-      return responseJson;
-    } else {
-      console.log(responseJson.message);
-      return responseJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const removeProjectLanguage = async (id: string, language: string) => {
-  const model = {
-    id: id,
-    language: language,
-  };
+const changeProjects = async (
+    projects:
+        ({text:string, id: string, type:'description' | 'title'}
+            | {operation:'add'|'remove', language: string, type:'language', id:string} |
+            {id: string, operation:'remove', type:'project'}
+            | {title: string, description: string, language: string, operation:'add', type:'project'}
+            )) => {
   try {
     const response = await fetch(
-      "http://localhost:8080/project/remove-language",
-      {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(model),
-        credentials: "include",
-      }
+        "https://f60z27ge89.execute-api.us-east-1.amazonaws.com/prod/projects",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage.getItem("Id"),
+          },
+          body: JSON.stringify(projects),
+        }
     );
-
-    const responseJson = await response.json();
-    if (responseJson.status === "OK") {
-      return responseJson;
-    } else {
-      console.log(responseJson.message);
-      return responseJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const addProjectLanguage = async (id: string, language: string) => {
-  const model = {
-    id: id,
-    language: language,
-  };
-  try {
-    const response = await fetch("http://localhost:8080/project/add-language", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
 
     const responseJson = await response.json();
     if (responseJson.status === "OK") {
@@ -154,10 +31,5 @@ const addProjectLanguage = async (id: string, language: string) => {
 };
 
 export {
-  removeProject,
-  addProject,
-  updateProjectTitle,
-  updateProjectDescription,
-  removeProjectLanguage,
-  addProjectLanguage,
+    changeProjects
 };
