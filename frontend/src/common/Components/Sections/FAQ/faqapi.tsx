4539114cce8dcc20212f2068f2a77256b11a8d09
noah s <noahsolomon2003@gmail.com>
2023-08-17 16:53:14 -0400

    faq api configured to be inside a single api endpoint

--- frontend/src/common/Components/Sections/FAQ/faqapi.tsx
+++ frontend/src/common/Components/Sections/FAQ/faqapi.tsx
@@ -1,59 +1,16 @@
-const updateTitleFaq = async (id: string, title: string) => {
-  const model = {
-    id: id,
-    title: title,
-  };
+const changeFaq = async (faq: ({text:string, type: 'title' | 'content', id:string} | {title: string, content: string, operation:'add', type:'faq'} | {id: string, operation:'remove', type:'faq'})) => {
   try {
-    const updateFetch = await fetch("http://localhost:8080/faq/title", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
-    const updateFetchJson = await updateFetch.json();
-    if (updateFetchJson.status === "OK") {
-      return updateFetchJson;
-    } else {
-      console.log(updateFetchJson.message);
-      return updateFetchJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const updateContentFaq = async (id: string, content: string) => {
-  const model = {
-    id: id,
-    content: content,
-  };
-  try {
-    const updateFetch = await fetch("http://localhost:8080/faq/content", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
-    const updateFetchJson = await updateFetch.json();
-    if (updateFetchJson.status === "OK") {
-      return updateFetchJson;
-    } else {
-      console.log(updateFetchJson.message);
-      return updateFetchJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const removeFaq = async (id: string) => {
-  try {
-    const response = await fetch("http://localhost:8080/faq/remove-faq", {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-      body: id,
-      credentials: "include",
-    });
+    const response = await fetch(
+        "https://f60z27ge89.execute-api.us-east-1.amazonaws.com/prod/faq",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage.getItem("Id"),
+          },
+          body: JSON.stringify(faq),
+        }
+    );
 
     const responseJson = await response.json();
     if (responseJson.status === "OK") {
@@ -67,29 +24,6 @@ const removeFaq = async (id: string) => {
   }
 };
 
-const addFaqPost = async (title: string, content: string) => {
-  const model = {
-    title: title,
-    content: content,
-  };
-  try {
-    const response = await fetch("http://localhost:8080/faq/add-faq", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
 
-    const responseJson = await response.json();
-    if (responseJson.status === "OK") {
-      return responseJson;
-    } else {
-      console.log(responseJson.message);
-      return responseJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
 
-export { updateTitleFaq, updateContentFaq, removeFaq, addFaqPost };
+export { changeFaq };
