0df8980cb939ed3641bd964b2abf8ffe35ba40d3
noah s <noahsolomon2003@gmail.com>
2023-08-16 20:26:48 -0400

    all job functions consolidated into job operations function

--- frontend/src/util/api/userapi.tsx
+++ frontend/src/util/api/userapi.tsx
@@ -15,10 +15,10 @@ const userDetails = async () => {
 
     const responseJson = await response.json();
     if (responseJson.status === "OK") {
-      return responseJson.data;
+      return responseJson;
     } else {
       console.log(responseJson);
-      return responseJson.data;
+      return responseJson;
     }
   } catch (e) {
     console.log(e);
@@ -179,201 +179,39 @@ const getProjectsPage = async () => {
   }
 };
 
-const removeJob = async (id: string) => {
-  try {
-    const response = await fetch("http://localhost:8080/remove-job", {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-      body: id,
-      credentials: "include",
-    });
+type JobOperation =
+  | { type: "remove"; id: number; order_id: number }
+  | {
+      type: "add";
+      company: string;
+      position: string;
+      description: string;
+      start_date: string;
+      end_date: string;
+      order_id: number;
+    }
+  | { type: "update"; field: string; id: number; value: string }
+  | { type: "orderChange"; from: number; to: number };
 
+const jobOperations = async (operation: JobOperation) => {
+  try {
+    const response = await fetch(
+      "https://f60z27ge89.execute-api.us-east-1.amazonaws.com/prod/resume/job",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("Id"),
+        },
+        body: JSON.stringify(operation),
+      }
+    );
     const responseJson = await response.json();
     if (responseJson.status === "OK") {
       return responseJson;
     } else {
       console.log(responseJson.message);
       return responseJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const addJob = async (
-  company: string,
-  position: string,
-  description: string,
-  startDate: string,
-  endDate: string,
-  orderId: number
-) => {
-  const model = {
-    company: company,
-    position: position,
-    description: description,
-    startDate: startDate,
-    endDate: endDate,
-    orderId: orderId,
-  };
-  try {
-    const response = await fetch("http://localhost:8080/add-job", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
-
-    const responseJson = await response.json();
-    if (responseJson.status === "OK") {
-      return responseJson;
-    } else {
-      console.log(responseJson.message);
-      return responseJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const updateJobCompany = async (id: string, company: string) => {
-  const model = {
-    id: id,
-    company: company,
-  };
-  try {
-    const updateFetch = await fetch("http://localhost:8080/job/company", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
-    const updateFetchJson = await updateFetch.json();
-    if (updateFetchJson.status === "OK") {
-      return updateFetchJson;
-    } else {
-      console.log(updateFetchJson.message);
-      return updateFetchJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const updateJobDescription = async (id: string, description: string) => {
-  const model = {
-    id: id,
-    description: description,
-  };
-  try {
-    const updateFetch = await fetch("http://localhost:8080/job/description", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
-    const updateFetchJson = await updateFetch.json();
-    if (updateFetchJson.status === "OK") {
-      return updateFetchJson;
-    } else {
-      console.log(updateFetchJson.message);
-      return updateFetchJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const updateJobRole = async (id: string, position: string) => {
-  const model = {
-    id: id,
-    position: position,
-  };
-  try {
-    const updateFetch = await fetch("http://localhost:8080/job/position", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
-    const updateFetchJson = await updateFetch.json();
-    if (updateFetchJson.status === "OK") {
-      return updateFetchJson;
-    } else {
-      console.log(updateFetchJson.message);
-      return updateFetchJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const updateJobStartDate = async (id: string, startDate: string) => {
-  const model = {
-    id: id,
-    startDate: startDate,
-  };
-  try {
-    const updateFetch = await fetch("http://localhost:8080/job/start-date", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
-    const updateFetchJson = await updateFetch.json();
-    if (updateFetchJson.status === "OK") {
-      return updateFetchJson;
-    } else {
-      console.log(updateFetchJson.message);
-      return updateFetchJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const updateJobEndDate = async (id: string, endDate: string) => {
-  const model = {
-    id: id,
-    endDate: endDate,
-  };
-  try {
-    const updateFetch = await fetch("http://localhost:8080/job/end-date", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
-    const updateFetchJson = await updateFetch.json();
-    if (updateFetchJson.status === "OK") {
-      return updateFetchJson;
-    } else {
-      console.log(updateFetchJson.message);
-      return updateFetchJson;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-const jobOrderChange = async (from: number, to: number) => {
-  const model = {
-    from: from,
-    to: to,
-  };
-  try {
-    const updateFetch = await fetch("http://localhost:8080/job/change-order", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(model),
-      credentials: "include",
-    });
-    const updateFetchJson = await updateFetch.json();
-    if (updateFetchJson.status === "OK") {
-      return updateFetchJson;
-    } else {
-      console.log(updateFetchJson.message);
-      return updateFetchJson;
     }
   } catch (e) {
     console.log(e);
@@ -387,12 +225,5 @@ export {
   getAbout,
   getContact,
   getProjectsPage,
-  removeJob,
-  addJob,
-  updateJobCompany,
-  updateJobDescription,
-  updateJobRole,
-  updateJobStartDate,
-  updateJobEndDate,
-  jobOrderChange,
+  jobOperations,
 };
