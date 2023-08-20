ca4a5a7ef2abf2f1bd5fed0790bdfddcc46ef2cb
noah s <noahsolomon2003@gmail.com>
2023-08-20 12:16:43 -0400

    endpoint to check if there is a website column value

--- frontend/src/util/api/deployapi.tsx
+++ frontend/src/util/api/deployapi.tsx
@@ -21,4 +21,26 @@ const deploy = async () => {
   }
 };
 
-export { deploy };
+const checkDeployed = async () => {
+  try {
+    const response = await fetch("https://f60z27ge89.execute-api.us-east-1.amazonaws.com/prod/deployed", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("Id")
+      },
+    });
+
+    const responseJson = await response.json();
+    if (responseJson.status === "OK") {
+      return responseJson;
+    } else {
+      console.log(responseJson.message);
+      return responseJson;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export { deploy, checkDeployed };
