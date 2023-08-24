9a7fda60b7f7354a316dff2b91b839080f91d909
noah s <noahsolomon2003@gmail.com>
2023-08-24 18:26:52 -0400

    aoi for checking subdomain availability

--- frontend/src/util/api/deployapi.tsx
+++ frontend/src/util/api/deployapi.tsx
@@ -27,6 +27,32 @@ const deploy = async (subdomain: string) => {
   }
 };
 
+const subdomainAvailability = async (subdomain: string) => {
+  try {
+    const response = await fetch(
+        `https://f60z27ge89.execute-api.us-east-1.amazonaws.com/${STAGE}/subdomain?subdomain=${subdomain}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage.getItem("Id"),
+          },
+        }
+    );
+
+    const responseJson = await response.json();
+    if (responseJson.status === "OK") {
+      return responseJson;
+    } else {
+      console.log(responseJson.message);
+      return responseJson;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+
 const checkDeployed = async () => {
   try {
     const response = await fetch(
@@ -52,4 +78,4 @@ const checkDeployed = async () => {
   }
 };
 
-export { deploy, checkDeployed };
+export { deploy, checkDeployed, subdomainAvailability };
