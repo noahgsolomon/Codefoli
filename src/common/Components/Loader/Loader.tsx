723300f07a252efb53d0a09ec070a89f40046f17
noah s <noahsolomon2003@gmail.com>
2023-08-27 15:45:05 -0400

    loader changed for dark mode: (component not in users portfolio)

--- src/common/Components/Loader/Loader.tsx
+++ src/common/Components/Loader/Loader.tsx
@@ -74,11 +74,11 @@ const Loader: FC = () => {
   );
 
   return (
-    <div className="fixed left-0 top-0 z-30 h-screen w-screen bg-gray-50">
+    <div className="fixed left-0 top-0 z-30 h-screen w-screen">
       <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center">
         <div role="status">
           <svg
-            className="mr-2 h-10 w-10 animate-spin rounded-full border-2 border-gray-200"
+            className="mr-2 h-10 w-10 animate-spin rounded-full border-2 border-gray-200 dark:border-gray-300"
             viewBox="0 0 24 24"
           >
             <circle
@@ -87,11 +87,11 @@ const Loader: FC = () => {
               cy="12"
               r="10"
               strokeWidth="4"
-              fill={"white"}
+              fill={localStorage.getItem('theme') === 'light' ? 'white' : '#1a1a1a'}
             ></circle>
             <path
               className="opacity-75"
-              fill="white"
+              fill={'white'}
               d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
             ></path>
           </svg>
