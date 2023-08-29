1182f04800a82715d73b2b62efc9c31541c7f96a
noah s <noahsolomon2003@gmail.com>
2023-08-28 20:02:50 -0400

    change 7: header totally different format plus themes

--- src/preview/common/Components/Header/HeaderP.tsx
+++ src/preview/common/Components/Header/HeaderP.tsx
@@ -1,48 +1,61 @@
-import { Link } from "react-router-dom";
-import { FC } from "react";
+import {Link} from "react-router-dom";
+import {FC, useState} from "react";
+import {toggleTheme} from "../../../../util/toggleTheme.ts";
 
 const HeaderP: FC = () => {
+
+  const [theme, setTheme] = useState<'light' | 'dark'>(localStorage.getItem('theme') as ('light' | 'dark') || 'light');
+
   return (
-    <header
-      className={`relative z-40 mx-5 flex items-center justify-center py-5 font-bold transition-all ease-linear md:flex-row`}
-    >
-      <div
-        className="mx-auto flex w-full flex-col items-center justify-between rounded-xl border-2 border-black bg-white px-4 py-3 shadow-custom transition-all md:flex-row"
-        style={{ maxWidth: "40rem" }}
+      <header
+          className={`relative z-40 mx-5 flex items-center py-5 font-bold transition-all`}
       >
-        <>
-          <div className="flex w-full flex-row items-center justify-center text-gray-800 md:flex-row md:text-base">
-            <div className="lg:mx-10">
-              <Link
-                to="/preview"
-                className="mx-4 py-1 text-sm  no-underline transition-all hover:text-blue-500 sm:text-xl sm:font-extrabold md:mx-10"
-              >
-                Home
-              </Link>
-              <Link
-                to="/preview/about"
-                className="mx-4 py-1 text-sm no-underline  transition-all hover:text-blue-500 sm:text-xl sm:font-extrabold md:mx-10"
-              >
-                About
-              </Link>
-              <Link
-                to="/preview/projects"
-                className="mx-4 py-1 text-sm no-underline transition-all hover:text-blue-500 sm:text-xl sm:font-extrabold md:mx-10"
-              >
-                Projects
-              </Link>
-              <Link
-                to="/preview/contact"
-                className="mx-4 py-1 text-sm no-underline transition-all hover:text-blue-500 sm:text-xl sm:font-extrabold md:mx-10"
-              >
-                Contact
-              </Link>
-            </div>
-          </div>
-        </>
-      </div>
-    </header>
+        <div
+            className="mx-auto flex w-full flex-row flex-wrap items-center justify-center sm:justify-between rounded-xl border-2 border-black
+ dark:bg-[#1a1a1a] px-4 py-3 shadow-custom transition-all max-w-[50rem]"
+        >
+
+                <Link
+                    to=""
+                    className="text-2xl mx-3 py-1 text-gray-800 dark:text-gray-50  no-underline transition-all dark:hover:text-blue-500 hover:text-blue-500"
+                >
+                  Home
+                </Link>
+                <Link
+                    to="about"
+                    className="py-1 mx-3 text-gray-800 dark:text-gray-50 text-2xl no-underline  transition-all dark:hover:text-blue-500 hover:text-blue-500"
+                >
+                  About
+                </Link>
+                <Link
+                    to="projects"
+                    className="py-1 mx-3 text-gray-800 dark:text-gray-50 text-2xl no-underline transition-all dark:hover:text-blue-500 hover:text-blue-500 "
+                >
+                  Projects
+                </Link>
+                <Link
+                    to="contact"
+                    className="py-1 mx-3 text-gray-800 dark:text-gray-50 text-2xl no-underline transition-all dark:hover:text-blue-500 hover:text-blue-500 "
+                >
+                  Contact
+                </Link>
+                <div
+                    onClick={() => {
+                      toggleTheme();
+                      setTheme(localStorage.getItem('theme') as ('light' | 'dark') || 'light');
+                    }}>
+                  {theme === 'light' ? (
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-5 h-5 cursor-pointer transition-all fill-yellow-500 hover:opacity-80">
+                        <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.844a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06 1.06l1.59-1.591a.75.75 0 00-1.061-1.06l-1.59 1.591z" />
+                      </svg>
+                  ) : (
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-5 h-5 cursor-pointer transition-all hover:opacity-80 fill-yellow-200">
+                        <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" clipRule="evenodd" />
+                      </svg>
+                  )}
+                </div>
+        </div>
+      </header>
   );
 };
-
 export default HeaderP;
