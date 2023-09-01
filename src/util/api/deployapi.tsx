96d6e04b8fa1ef32ef4dbcb84f6a5b62c84cb65e
noah s <noahsolomon2003@gmail.com>
2023-09-01 09:51:29 -0400

    added distribution to deploy api

--- src/util/api/deployapi.tsx
+++ src/util/api/deployapi.tsx
@@ -1,6 +1,6 @@
 import { STAGE } from "../../config.ts";
 
-const deploy = async ({ subdomain, custom_domain }: { subdomain: string | null, custom_domain: string | null }) => {
+const deploy = async ({ subdomain, custom_domain, distribution }: { subdomain: string | null, custom_domain: string | null, distribution: string | null }) => {
   try {
     const response = await fetch(
       `https://f60z27ge89.execute-api.us-east-1.amazonaws.com/${STAGE}/deploy`,
@@ -10,7 +10,7 @@ const deploy = async ({ subdomain, custom_domain }: { subdomain: string | null, 
           "Content-Type": "application/json",
           Authorization: "Bearer " + localStorage.getItem("Id"),
         },
-        body: JSON.stringify({ subdomain: subdomain, custom_domain: custom_domain }),
+        body: JSON.stringify({ subdomain: subdomain, custom_domain: custom_domain, distribution: distribution }),
       }
     );
 
@@ -77,4 +77,29 @@ const checkDeployed = async () => {
   }
 };
 
-export { deploy, checkDeployed, subdomainAvailability };
+const checkCustomDomainDetails = async () => {
+    try {
+        const response = await fetch(
+        `https://f60z27ge89.execute-api.us-east-1.amazonaws.com/${STAGE}/custom-domain`,
+        {
+            method: "GET",
+            headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage.getItem("Id"),
+            },
+        }
+        );
+
+        const responseJson = await response.json();
+        if (responseJson.status === "OK") {
+        return responseJson;
+        } else {
+        console.log(responseJson.message);
+        return responseJson;
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export { deploy, checkDeployed, subdomainAvailability, checkCustomDomainDetails };
