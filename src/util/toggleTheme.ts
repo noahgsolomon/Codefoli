bd9238848a54972d6438b49950e2e9161fe4e712
noah s <noahsolomon2003@gmail.com>
2023-09-02 10:42:49 -0400

    event so listeners know when theme local storage is changed

--- src/util/toggleTheme.ts
+++ src/util/toggleTheme.ts
@@ -6,10 +6,14 @@ export const toggleTheme = () => {
         document.body.classList.remove(DARK_THEME_KEY);
         document.body.classList.add(LIGHT_THEME_KEY);
         window.localStorage.setItem(LOCALSTORAGE_THEME_KEY, LIGHT_THEME_KEY);
+        const themeChangedEvent = new Event('themeChanged');
+        window.dispatchEvent(themeChangedEvent);
     }
     else {
         document.body.classList.add(DARK_THEME_KEY);
         document.body.classList.remove(LIGHT_THEME_KEY);
         window.localStorage.setItem(LOCALSTORAGE_THEME_KEY, DARK_THEME_KEY);
+        const themeChangedEvent = new Event('themeChanged');
+        window.dispatchEvent(themeChangedEvent);
     }
 }
