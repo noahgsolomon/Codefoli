621f46bbeaccc36be30d0a7859ac4b416c3197bf
noah s <noahsolomon2003@gmail.com>
2023-09-19 12:40:52 -0400

    theme card for each theme option

---
+++ src/theme/pages/Themes/ThemeCard.tsx
@@ -0,0 +1,36 @@
+import {DARK_THEME_KEY} from "../../../util/constants.ts";
+import {FC} from "react";
+
+
+const ThemeCard: FC<{
+    themeTitle: string;
+    imgLight: string;
+    imgDark: string;
+    currentTheme: string | null;
+}> = ({themeTitle, imgLight, imgDark, currentTheme}) => {
+
+    return (
+        <div
+            className="m-5  max-w-[400px] flex flex-col rounded-xl border-2 border-black shadow-custom transition-all hover:-translate-y-0.5 hover:shadow-customHover"
+        >
+            <div
+                className="image-wrapper relative h-[400px] overflow-hidden rounded-t-lg transition-all"
+            >
+                <img
+                    src={
+                        currentTheme === DARK_THEME_KEY ? imgDark : imgLight
+                    }
+                    alt="some"
+                    className={`inline-block h-full w-full transform object-cover transition-all ease-in-out`}
+                />
+            </div>
+            <div className="bg-white p-5 rounded-b-lg dark:bg-[#0d0d0d]">
+                <h2 className="mb-5  text-2xl font-bold leading-snug transition-all">
+                    {themeTitle}
+                </h2>
+            </div>
+        </div>
+    );
+};
+
+export default ThemeCard;
