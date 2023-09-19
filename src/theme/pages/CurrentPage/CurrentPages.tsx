54e0bf5b544b2288ed17b3564e6fdbbf7022f352
noah s <noahsolomon2003@gmail.com>
2023-09-19 12:39:40 -0400

    current page component for all current builds

---
+++ src/theme/pages/CurrentPage/CurrentPages.tsx
@@ -0,0 +1,19 @@
+import {FC} from "react";
+import gapFilledDark from "assets/gap-filled-dark.png";
+import gapFilledLight from "assets/gap-filled-light.png";
+import PageCard from "./PageCard.tsx";
+const CurrentPages: FC<{
+    currentTheme: string | null;
+}> = ({currentTheme}) => {
+
+    return (
+        <div className={'w-full'}>
+        <h1 className={'text-center text-3xl font-bold'}>Current Pages</h1>
+            <div className={'w-full flex justify-center flex-wrap gap-4'}>
+                <PageCard themeTitle={'Paper'} currentTheme={currentTheme} imgDark={gapFilledDark} imgLight={gapFilledLight}/>
+            </div>
+        </div>
+    );
+};
+
+export default CurrentPages;
