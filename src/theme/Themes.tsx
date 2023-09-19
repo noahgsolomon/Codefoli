062ca82f689a3141088d167036ed90f606c923f7
noah s <noahsolomon2003@gmail.com>
2023-09-19 12:41:46 -0400

    themes which is going to be the home page for authenticated codefoli.com users. Kinda like a launch hub

--- src/theme/Themes.tsx
+++ src/theme/Themes.tsx
@@ -1,11 +1,40 @@
-import { FC } from "react";
+import {FC, useEffect, useState} from "react";
 import Footer from "./Footer.tsx";
 import Header from "./Header.tsx";
+import CurrentPages from "./pages/CurrentPage/CurrentPages.tsx";
+import {DARK_THEME_KEY, LIGHT_THEME_KEY, LOCALSTORAGE_THEME_KEY} from "../util/constants.ts";
+import ThemePages from "./pages/Themes/ThemePages.tsx";
 const Themes: FC = () => {
+
+    const [theme, setTheme] = useState<
+        typeof LIGHT_THEME_KEY | typeof DARK_THEME_KEY
+    >(
+        (localStorage.getItem(LOCALSTORAGE_THEME_KEY) as
+            | typeof LIGHT_THEME_KEY
+            | typeof DARK_THEME_KEY) || LIGHT_THEME_KEY
+    );
+
+    const [currentTheme, setCurrentTheme] = useState(
+        localStorage.getItem(LOCALSTORAGE_THEME_KEY)
+    );
+
+    useEffect(() => {
+        const themeChangeListener = () => {
+            setCurrentTheme(localStorage.getItem("theme"));
+        };
+
+        window.addEventListener("themeChanged", themeChangeListener);
+
+        return () => {
+            window.removeEventListener("themeChanged", themeChangeListener);
+        };
+    }, []);
 
     return (
         <>
-            <Header />
+            <Header setTheme={setTheme} theme={theme}/>
+            <CurrentPages currentTheme={currentTheme}/>
+            <ThemePages currentTheme={currentTheme} />
             <Footer />
         </>
     );
