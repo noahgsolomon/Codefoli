c0414a6849470dca2492b58b30c559ebe887458a
noah s <noahsolomon2003@gmail.com>
2023-09-20 10:06:40 -0400

    looping through pages but just as paper for right now

--- src/theme/pages/CurrentPage/CurrentPages.tsx
+++ src/theme/pages/CurrentPage/CurrentPages.tsx
@@ -4,16 +4,20 @@ import gapFilledLight from "assets/gap-filled-light.png";
 import PageCard from "./PageCard.tsx";
 const CurrentPages: FC<{
   currentTheme: string | null;
-}> = ({ currentTheme }) => {
+  themes: string[];
+}> = ({ currentTheme, themes }) => {
   return (
     <div className={"w-full"}>
       <h1 className={"text-center text-3xl font-bold"}>Current Pages</h1>
       <div className={"flex w-full flex-wrap justify-center gap-4"}>
-        <PageCard
-          currentTheme={currentTheme}
-          imgDark={gapFilledDark}
-          imgLight={gapFilledLight}
-        />
+        {themes.map((theme, index) => (
+            theme === 'PAPER' ? (<PageCard
+                key={index}
+                currentTheme={currentTheme}
+                imgDark={gapFilledDark}
+                imgLight={gapFilledLight}
+            />) : null
+            ))}
       </div>
     </div>
   );
