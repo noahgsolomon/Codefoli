5da7223f434039b84ce0b258fbba7e831781141c
noah s <noahsolomon2003@gmail.com>
2023-09-20 10:07:53 -0400

    style for if theme is already being used

--- src/theme/pages/Themes/ThemeCard.tsx
+++ src/theme/pages/Themes/ThemeCard.tsx
@@ -1,25 +1,34 @@
 import { DARK_THEME_KEY } from "../../../util/constants.ts";
 import { FC, useState } from "react";
-import { FiArrowUpRight } from "react-icons/fi";
+import {FiArrowUpRight, FiLock} from "react-icons/fi";
 
 const ThemeCard: FC<{
   themeTitle: string;
   imgLight: string;
   imgDark: string;
   currentTheme: string | null;
-}> = ({ themeTitle, imgLight, imgDark, currentTheme }) => {
+  used: boolean;
+}> = ({ themeTitle, imgLight, imgDark, currentTheme, used }) => {
   const [hovered, setHovered] = useState(false);
 
   return (
     <div
-      className="m-5 flex max-h-[400px] max-w-[400px] cursor-pointer flex-col rounded-xl border-2 border-black shadow-custom transition-all hover:-translate-y-0.5 hover:shadow-customHover"
+      className={`${used ? 'opacity-70' : ''} relative m-5 flex max-h-[400px] max-w-[400px] cursor-pointer flex-col rounded-xl border-2 border-black shadow-custom transition-all hover:-translate-y-0.5 hover:shadow-customHover`}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
       <div
-        className={`transition-visible absolute inset-0 z-10 flex items-center justify-center rounded-lg border-8 border-dashed border-black border-opacity-30 bg-green-500 opacity-0 transition-opacity ${
-          hovered ? "visible opacity-80" : "invisible"
-        }`}
+          className={`absolute inset-0 z-10 flex items-center justify-center rounded-lg border-8 border-dashed border-black border-opacity-30 bg-black ${
+              used ? "visible opacity-60" : "hidden"
+          }`}
+      >
+        <FiLock className="text-4xl text-white" />
+        <h3 className={"text-4xl font-bold text-white"}>Theme used</h3>
+      </div>
+      <div
+          className={`transition-visible absolute inset-0 z-10 flex items-center justify-center rounded-lg border-8 border-dashed border-black border-opacity-30 bg-green-500 opacity-0 transition-opacity ${
+              !used && hovered ? "visible opacity-80" : "invisible"
+          }`}
       >
         <h3 className={"text-4xl font-bold text-white"}>Click to Build Page</h3>
         <FiArrowUpRight className={"ml-4 text-4xl text-white"} />
