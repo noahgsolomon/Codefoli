167cbd4f95cb38efdf7431a658dab063dcd76f81
noah s <noahsolomon2003@gmail.com>
2023-09-24 17:18:25 -0400

    centering vertically login and theme button

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -52,8 +52,8 @@ const Home: FC = () => {
         <div className="h-screen">
           <div className="md:h-48 h-20 bg-gradient-to-b from-purple-100 to-transparent dark:from-gray-900">
             <header>
-              <div className={'pt-10 mx-20 justify-between flex flex-row'}>
-                <div onClick={handleToggleTheme} className={'cursor-pointer w-10 h-10'}>
+              <div className={'pt-10 mx-20 justify-between items-center flex flex-row'}>
+                <div onClick={handleToggleTheme} className={'cursor-pointer'}>
                   {theme === LIGHT_THEME_KEY ? (
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-5 w-5" aria-hidden="true"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></svg>
                   ) : (
