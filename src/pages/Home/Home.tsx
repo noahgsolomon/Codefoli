c43de765cc4c7a96b238a5dbb451f6d78f191717
Noah Solomon <noahsolomon2003@gmail.com>
2023-09-27 14:03:58 -0400

    new home style hero

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,7 +7,7 @@ import {
 import { toggleTheme } from "../../util/toggleTheme.ts";
 import { Button } from "./button.tsx";
 import { Link } from "react-router-dom";
-import { Github, Shuffle, Star } from "../../util/ui/icons.ts";
+import { Shuffle } from "../../util/ui/icons.ts";
 import ratatoonie from "assets/ratatoonieprofile.png";
 import noah from "assets/noahprofile.png";
 import walterwhite from "assets/walterwhiteprofile.png";
@@ -26,83 +26,90 @@ import FeatureCard from "./FeatureCard.tsx";
 import addEmail from "api/newsletterapi.tsx";
 import { BsDiscord } from "react-icons/bs";
 import { useSpring, animated } from "react-spring";
+import { Frame, Sparkles } from "lucide-react";
 
 const purpleTheme = {
   primary: "#9b59b6",
   secondary: "#643a79",
   headerGradient: "from-purple-100 to-transparent",
-  buttonGradient: "from-purple-500 to-purple-800",
+  buttonGradient: 'from-purple-500 to-purple-800 dark:to-purple-300',
   ring: "ring-purple-600",
   buttonFill: "fill-purple-500",
   color1: "#d8b4fe",
   color2: "#a855f7",
   color3: "#7c3aed",
   color4: "#4c1d95",
+  text: 'text-purple-500'
 };
 
 const yellowTheme = {
   primary: "#f1c40f",
   secondary: "#f39c12",
   headerGradient: "from-yellow-100 to-transparent",
-  buttonGradient: "from-yellow-500 to-yellow-800",
+  buttonGradient: "from-yellow-500 to-yellow-800 dark:to-yellow-300",
   ring: "ring-yellow-600",
   buttonFill: "fill-yellow-500",
   color1: "#fff200", // Lightest Yellow
   color2: "#ffcc00", // Light Yellow
   color3: "#ff9900", // Medium Yellow-Orange
   color4: "#cc6600", // Dark Orange-Brown
+  text: 'text-yellow-500'
 };
 
 const orangeTheme = {
   primary: "#e67e22",
   secondary: "#d35400",
   headerGradient: "from-orange-100 to-transparent",
-  buttonGradient: "from-orange-500 to-orange-800",
+  buttonGradient: "from-orange-500 to-orange-800 dark:to-orange-300",
   ring: "ring-orange-600",
   buttonFill: "fill-orange-500",
   color1: "#ffad42", // Lightest Orange
   color2: "#ff8000", // Light Orange
   color3: "#ff5500", // Medium Orange-Red
   color4: "#cc4400", // Darkest Reddish-Brown
+  text: 'text-orange-500'
 };
 
 const greenTheme = {
   primary: "#2ecc71",
   secondary: "#27ae60",
   headerGradient: "from-green-100 to-transparent",
-  buttonGradient: "from-green-500 to-green-800",
+  buttonGradient: "from-green-500 to-green-800 dark:to-green-300",
   ring: "ring-green-600",
   buttonFill: "fill-green-500",
   color1: "#6ab04c",
   color2: "#26de81",
   color3: "#10ac84",
   color4: "#0a3d62",
+  text: 'text-green-500'
 };
 
 const blueTheme = {
   primary: "#3498db",
   secondary: "#2980b9",
   headerGradient: "from-blue-100 to-transparent",
-  buttonGradient: "from-blue-500 to-blue-800",
+  buttonGradient: "from-blue-500 to-blue-800 dark:to-blue-300",
   ring: "ring-blue-600",
   buttonFill: "fill-blue-500",
   color1: "#add8e6", // Lightest Blue
   color2: "#0099cc", // Light Blue
   color3: "#006699", // Medium Blue
   color4: "#003366", // Darkest Blue
+  text: 'text-blue-500'
 };
 
 const redTheme = {
   primary: "#ff5349",
   secondary: "#b33a2d",
   headerGradient: "from-red-100 to-transparent",
-  buttonGradient: "from-red-500 to-red-800",
+  buttonGradient: "from-red-500 to-red-800 dark:to-red-300",
   ring: "ring-red-600",
   buttonFill: "fill-red-500",
   color1: "#ff6666", // Lightest Red
   color2: "#ff3333", // Light Red
   color3: "#cc0000", // Medium Red
   color4: "#990000", // Darkest Red
+  text: 'text-red-500'
 };
 
 const Home: FC = () => {
@@ -289,82 +296,80 @@ const Home: FC = () => {
                 "mx-5 flex flex-row items-center justify-between pt-10 md:mx-20"
               }
             >
-              <div className={"flex cursor-pointer flex-row gap-4"}>
-                {theme === LIGHT_THEME_KEY ? (
-                  <svg
-                    onClick={handleToggleTheme}
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="h-5 w-5"
-                    aria-hidden="true"
-                  >
-                    <circle cx="12" cy="12" r="4"></circle>
-                    <path d="M12 2v2"></path>
-                    <path d="M12 20v2"></path>
-                    <path d="m4.93 4.93 1.41 1.41"></path>
-                    <path d="m17.66 17.66 1.41 1.41"></path>
-                    <path d="M2 12h2"></path>
-                    <path d="M20 12h2"></path>
-                    <path d="m6.34 17.66-1.41 1.41"></path>
-                    <path d="m19.07 4.93-1.41 1.41"></path>
-                  </svg>
-                ) : (
-                  <svg
-                    onClick={handleToggleTheme}
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="h-5 w-5"
-                    aria-hidden="true"
-                  >
-                    <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
-                  </svg>
-                )}
-                <div onClick={togglePageTheme} className={"cursor-pointer"}>
-                  <Shuffle />
-                </div>
+              <div className={"flex cursor-pointer items-center flex-row gap-4"}>
+                <Frame className="h-5 w-5 lg:h-10 lg:w-10 opacity-80 hover:opacity-60 transition-all" />
               </div>
-              {/*<div className="flex flex-col items-center justify-center ">*/}
-              {/*  <div className="mt-1 flex flex-col gap-3 px-4 text-center sm:px-0">*/}
-              {/*    <Link*/}
-              {/*        className="mx-auto rounded-full bg-gradient-to-r from-gray-400 to-gray-600 p-[1px] brightness-90 contrast-150 focus:outline-none focus-visible:ring-2 dark:brightness-125 dark:contrast-100 dark:text-gray-200 sm:block"*/}
-              {/*        to={'/login'}*/}
-              {/*    >*/}
-              {/*      <div className="group relative overflow-hidden rounded-full bg-white/80 px-3 py-1 duration-300 hover:pr-9 dark:bg-black/80 dark:text-gray-300">*/}
-              {/*        <span className="bg-gradient-to-r from-gray-400 to-gray-800 bg-clip-text text-transparent block sm:inline dark:text-gray-300">*/}
-              {/*          <svg*/}
-              {/*              className="mr-1 inline-block h-4 w-4 fill-gray-600 dark:fill-gray-300"*/}
-              {/*              viewBox="0 0 24 24"*/}
-              {/*              xmlns="http://www.w3.org/2000/svg"*/}
-              {/*          >*/}
-              {/*            <path d="M12 2C8.13 2 5 5.13 5 9v2H3v10h18V11h-2V9c0-3.87-3.13-7-7-7zm1 14h-2v-2h2v2zm3-4H8V9c0-2.21 1.79-4 4-4s4 1.79 4 4v3z"/>*/}
-              {/*          </svg>*/}
-              {/*          Log in*/}
-              {/*         <img*/}
-              {/*             className="absolute -bottom-1 right-1 duration-300 sm:translate-y-7 group-hover:translate-y-0"*/}
-              {/*             alt="Among Us character"*/}
-              {/*             height="30"*/}
-              {/*             width="30"*/}
-              {/*             src={whiteamogus}*/}
-              {/*         />*/}
-              {/*        </span>*/}
-              {/*      </div>*/}
-              {/*    </Link>*/}
-              {/*  </div>*/}
-              {/*</div>*/}
+              <div className="flex flex-row gap-4">
+                {theme === LIGHT_THEME_KEY ? (
+                      <svg
+                        onClick={handleToggleTheme}
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="h-5 w-5 lg:h-7 lg:w-7 hover:opacity-80 transition-all cursor-pointer"
+                        aria-hidden="true"
+                      >
+                        <circle cx="12" cy="12" r="4"></circle>
+                        <path d="M12 2v2"></path>
+                        <path d="M12 20v2"></path>
+                        <path d="m4.93 4.93 1.41 1.41"></path>
+                        <path d="m17.66 17.66 1.41 1.41"></path>
+                        <path d="M2 12h2"></path>
+                        <path d="M20 12h2"></path>
+                        <path d="m6.34 17.66-1.41 1.41"></path>
+                        <path d="m19.07 4.93-1.41 1.41"></path>
+                      </svg>
+                    ) : (
+                      <svg
+                        onClick={handleToggleTheme}
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="h-5 w-5 lg:h-7 lg:w-7 hover:opacity-80 transition-all cursor-pointer"
+                        aria-hidden="true"
+                      >
+                        <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
+                      </svg>
+                    )}
+                    <div onClick={togglePageTheme} className={"cursor-pointer"}>
+                      <Shuffle className="h-5 w-5 lg:h-7 lg:w-7 hover:opacity-80 transition-all"/>
+                    </div>
+              </div>
+              <div className="justify-center items-center flex flex-row gap-2 lg:gap-4">
+                  <div className="flex flex-col items-center justify-center ">
+                    <div className="mt-1 flex flex-col gap-3 px-4 text-center sm:px-0">
+                      <Link
+                        className="mx-auto opacity-60 rounded-full bg-gradient-to-r from-gray-400 to-gray-600 p-[1px] brightness-90 contrast-150 focus:outline-none focus-visible:ring-2 dark:brightness-125 dark:contrast-100 dark:text-gray-200 sm:block"
+                        to={'/waitlist'}
+                      >
+                        <div className="group bg-gradient-to-r from-red-400 to-red-800 relative overflow-hidden rounded-full bg-white/80 px-3 py-1 duration-300 hover:pr-9 dark:bg-black/80">
+                          <span className="bg-gradient-to-r from-white to-gray-100 bg-clip-text text-transparent block sm:inline">
+                            Don't Click me!
+                            <img
+                              className="absolute -bottom-1 right-1 duration-300 sm:translate-y-7 group-hover:translate-y-0"
+                              alt="Among Us character"
+                              height="30"
+                              width="30"
+                              src={whiteamogus}
+                            />
+                          </span>
+                        </div>
+                      </Link>
+                    </div>
+                  </div>
+              </div>
             </div>
           </header>
         </div>
@@ -374,9 +379,9 @@ const Home: FC = () => {
             <div className="container mb-10 flex flex-col items-center justify-between xl:flex-row">
               <div className="mb-10 flex flex-col items-center justify-center gap-10 xl:items-start">
                 <div className="relative flex w-full items-center justify-center gap-4 xl:justify-start">
-                  <h1 className="bg-gradient-to-r from-gray-400 to-black bg-clip-text text-6xl font-extrabold leading-[5.5rem] text-transparent dark:to-white sm:text-8xl">
-                    Codefoli
-                  </h1>
+                <h1 className={`bg-gradient-to-r bg-clip-text text-6xl ${activeTheme.buttonGradient} font-extrabold leading-[5.5rem] text-transparent sm:text-8xl`}>
+                  Codefoli
+                </h1>
                 </div>
                 <p className="max-w-[55ch] bg-transparent px-8 text-center font-medium leading-8 text-black/60 dark:text-white/50 xl:px-0 xl:text-left">
                   <span ref={el}></span>
@@ -387,8 +392,9 @@ const Home: FC = () => {
                     className={`hero-join-button rounded-xl p-[2px] transition-all duration-300 hover:bg-transparent hover:shadow-[0_0_2rem_-0.5rem_#333333] dark:hidden`}
                   >
                     <Link to="/waitlist">
-                      <span className="inline-flex h-full w-fit items-center gap-1 rounded-[10px] bg-white px-12 py-2 text-gray-800 opacity-90 transition-all duration-300 md:px-4">
-                        Join Waitlist →
+                      <span className={`inline-flex h-full ${activeTheme.text} w-fit font-bold items-center gap-1 rounded-[10px] bg-white px-12 py-2 text-gray-800 opacity-90 transition-all duration-300 md:px-4`}>
+                        <Sparkles className="h-4 w-4"/>
+                        Join Waitlist
                       </span>
                     </Link>
                   </Button>
@@ -397,8 +403,9 @@ const Home: FC = () => {
                     className="  hero-join-button hidden rounded-xl p-[2px] transition-all duration-300 dark:block dark:hover:shadow-[0_0_2rem_-0.5rem_#fff8]"
                   >
                     <Link to="/waitlist">
-                      <span className="inline-flex h-full w-fit items-center gap-1 rounded-[10px] px-12 py-2 opacity-90 transition-all duration-300 dark:bg-neutral-900 dark:text-white md:px-4">
-                        Join Waitlist →
+                      <span className="inline-flex h-full w-fit font-bold items-center gap-1 rounded-[10px] px-12 py-2 opacity-90 transition-all duration-300 dark:bg-neutral-900 dark:text-white md:px-4">
+                      <Sparkles className="h-4 w-4"/>
+                        Join Waitlist
                       </span>
                     </Link>
                   </Button>
@@ -410,27 +417,11 @@ const Home: FC = () => {
                     <a
                       target="_blank"
                       rel="noreferrer"
-                      className="gap-1 md:inline-flex"
+                      className="gap-1 md:inline-flex font-bold opacity-80"
                       href="https://github.com/noahgsolomon/codefoli"
                     >
-                      <Github className="h-4 w-4" />
+                      <i className="fa-solid fa-star h-4 w-4"></i>
                       Star on Github
-                      <Star className="h-4 w-4 fill-yellow-500 opacity-80" />
-                    </a>
-                  </Button>
-                  <Button
-                    asChild
-                    className="flex items-center gap-2 rounded-xl border-2 px-4 py-2 dark:text-white"
-                    variant="outline"
-                  >
-                    <a
-                      target="_blank"
-                      rel="noreferrer"
-                      className="gap-1 md:inline-flex"
-                      href="https://twitter.com/noahgsolomon"
-                    >
-                      <i className="fa-brands fa-x-twitter h-4 w-4"></i>
-                      Twitter
                     </a>
                   </Button>
                 </div>
