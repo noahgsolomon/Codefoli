068ac96a994425b153246b325fec213ca3bb48fd
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-02 14:40:15 -0400

    themes changes

--- src/theme/Themes.tsx
+++ src/theme/Themes.tsx
@@ -1,5 +1,4 @@
 import { FC, useEffect, useState } from "react";
-import Header from "./Header.tsx";
 import CurrentPages from "./Components/CurrentPage/CurrentPages.tsx";
 import {
   DARK_THEME_KEY,
@@ -7,8 +6,8 @@ import {
   LOCALSTORAGE_THEME_KEY,
 } from "../util/constants.ts";
 import ThemePages from "./Components/Themes/ThemePages.tsx";
-import Sidebar from "./Sidebar.tsx";
-import Footer from "./Footer.tsx";
+import { toggleTheme } from "../util/toggleTheme.ts";
+import { Frame } from "lucide-react";
 const Themes: FC<{
   themes: {
     theme: string;
@@ -44,17 +43,77 @@ const Themes: FC<{
     };
   }, []);
 
+
+  const handleToggleTheme = () => {
+    toggleTheme();
+    setTheme(
+      (localStorage.getItem(LOCALSTORAGE_THEME_KEY) as
+        | typeof LIGHT_THEME_KEY
+        | typeof DARK_THEME_KEY) || typeof LIGHT_THEME_KEY
+    );
+  };
+
+
   return (
-    <>
-      <div className="mb-20 flex">
-        <Sidebar />
-        <div className="flex w-full flex-col">
-          <Header setTheme={setTheme} theme={theme} />
-          <CurrentPages themes={themes} />
-          <ThemePages currentTheme={currentTheme} themes={themes} />
-        </div>
-      </div>
-      <Footer />
+   <>
+    <header>
+            <div
+              className={
+                "mx-5 flex flex-row items-center justify-between pt-10 md:mx-20"
+              }
+            >
+              <div className={"cursor-pointer flex items-center flex-row gap-4"}>
+                <Frame className="h-5 w-5 lg:h-10 lg:w-10 opacity-80 hover:opacity-60 transition-all" />
+              </div>
+              <h2 className="font-bold text-4xl lg:text-6xl bg-gradient-to-r from-gray-400 to-gray-200 dark:from-gray-500 dark:to-gray-700 text-transparent bg-clip-text">Codefoli</h2>              <div className="flex flex-row gap-4">
+                {theme === LIGHT_THEME_KEY ? (
+                      <svg
+                        onClick={handleToggleTheme}
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="cursor-pointer h-5 w-5 hover:opacity-80 transition-all"
+                        aria-hidden="true"
+                      >
+                        <circle cx="12" cy="12" r="4"></circle>
+                        <path d="M12 2v2"></path>
+                        <path d="M12 20v2"></path>
+                        <path d="m4.93 4.93 1.41 1.41"></path>
+                        <path d="m17.66 17.66 1.41 1.41"></path>
+                        <path d="M2 12h2"></path>
+                        <path d="M20 12h2"></path>
+                        <path d="m6.34 17.66-1.41 1.41"></path>
+                        <path d="m19.07 4.93-1.41 1.41"></path>
+                      </svg>
+                    ) : (
+                      <svg
+                        onClick={handleToggleTheme}
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="cursor-pointer h-5 w-5 hover:opacity-80 transition-all"
+                        aria-hidden="true"
+                      >
+                        <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
+                      </svg>
+                    )}
+              </div>
+            </div>
+          </header>
+      <CurrentPages themes={themes} />
+      <ThemePages currentTheme={currentTheme} themes={themes} />
     </>
   );
 };
