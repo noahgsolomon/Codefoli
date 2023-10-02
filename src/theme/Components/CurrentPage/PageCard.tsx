efbf681ab8831f4048e1450206d9bd35c4437251
Noah Solomon <noahsolomon2003@gmail.com>
2023-10-02 14:40:30 -0400

    page card changes

--- src/theme/Components/CurrentPage/PageCard.tsx
+++ src/theme/Components/CurrentPage/PageCard.tsx
@@ -14,16 +14,16 @@ const PageCard: FC<{
   link: string;
 }> = ({ link, themes }) => {
   const [hovered, setHovered] = useState(false);
-  const [removeHover, setRemoveHover] = useState(false);
+  // const [removeHover, setRemoveHover] = useState(false);
 
   return (
     <div
-      className="shadow-custom hover:shadow-customHover m-5 flex max-h-[400px] max-w-[400px] cursor-pointer flex-col rounded-xl border-2 border-black transition-all hover:-translate-y-0.5"
+      className="cursor-pointer shadow-blue-100 shadow-2xl hover:shadow-3xl dark:shadow-gray-900 dark:border-gray-800 m-5 flex max-h-[400px] border-2 border-blue-100 border-opacity-50 max-w-[400px] flex-col rounded-xl transition-all hover:-translate-y-0.5"
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
       onClick={() => (window.location.href = link)}
     >
-      <button
+      {/* <button
         className={`${
           hovered ? "opacity-100" : "opacity-0"
         } absolute -right-4 -top-8 z-20 mt-5 rounded-2xl bg-red-500 px-5 font-bold text-white transition-all hover:-translate-y-0.5 hover:scale-105`}
@@ -31,25 +31,16 @@ const PageCard: FC<{
         onMouseLeave={() => setRemoveHover(false)}
       >
         -
-      </button>
+      </button> */}
 
       <div
         className={`transition-visible absolute inset-0 z-10 flex items-center justify-center rounded-lg border-8 border-dashed border-black border-opacity-30 bg-blue-500 opacity-0 transition-opacity ${
-          hovered && !removeHover ? "visible opacity-80" : "invisible"
+          hovered ? "visible opacity-80" : "invisible"
         }`}
       >
         <h3 className={"text-3xl font-bold text-white"}>
           Click to Enter Editor
         </h3>
-        <FiArrowUpRight className={"ml-4 text-3xl text-white"} />
-      </div>
-
-      <div
-        className={`transition-visible absolute inset-0 z-10 flex items-center justify-center rounded-lg border-8 border-dashed border-black border-opacity-30 bg-red-500 opacity-0 transition-opacity ${
-          removeHover ? "visible opacity-80" : "invisible"
-        }`}
-      >
-        <h3 className={"text-3xl font-bold text-white"}>Remove Page</h3>
         <FiArrowUpRight className={"ml-4 text-3xl text-white"} />
       </div>
       <div className=" relative h-[400px] overflow-hidden rounded-t-lg transition-all">
@@ -100,27 +91,18 @@ const PageCard: FC<{
         </div>
       </div>
       <div className="rounded-b-lg bg-white p-5 dark:bg-[#0d0d0d]">
-        <div className={"mt-2 flex flex-wrap gap-1"}>
-          <div
-            className={
-              "rounded-xl border-2 border-black border-opacity-30 bg-green-300 p-1 text-sm text-gray-50 dark:bg-green-900"
-            }
-          >
-            {themes.deployed ? "deployed" : "not deployed"}
+        <div className="mt-2 flex flex-wrap gap-1">
+          <div className={`${themes.deployed ? 'dark:text-green-600 from-green-400 to-green-300  text-green-900 dark:from-green-900 dark:to-green-800' : 'dark:text-red-600 from-red-400 to-red-300 text-red-900 dark:from-red-900 dark:to-red-800'} rounded-2xl bg-gradient-to-r border-2 border-black flex gap-2 items-center border-opacity-30 px-3 py-1 text-sm font-bold text-opacity-60`}>
+            <i className={`${themes.deployed ? 'dark:text-green-600 text-green-900' : 'dark:text-red-600 text-red-900'} opacity-60 dark:opacity-100 fa-solid fa-power-off`}></i>
+            {themes.deployed ? "Published" : "Not Published"}
           </div>
-          <div
-            className={
-              "rounded-xl border-2 border-black border-opacity-30 bg-red-300 p-1 text-sm text-gray-50 dark:bg-red-900"
-            }
-          >
-            last accessed: {themes.last_accessed || "before v1.6"}
+          <div className="bg-gradient-to-r text-cyan-800 text-opacity-60 dark:text-opacity-100 font-bold rounded-2xl flex items-center gap-2 border-2 border-black border-opacity-30 from-cyan-300 to-cyan-200 px-3 py-1 text-sm dark:text-opacity-100 dark:text-cyan-600 dark:from-cyan-900 dark:to-cyan-800">
+            <i className="text-cyan-800 dark:opacity-100 dark:text-cyan-600 opacity-60 fa-solid fa-clock-rotate-left"></i>
+            Latest: {themes.last_accessed || "before v1.6"}
           </div>
-          <div
-            className={
-              "rounded-xl border-2 border-black border-opacity-30 bg-blue-300 p-1 text-sm text-gray-50 dark:bg-blue-900"
-            }
-          >
-            domain: {themes.domain || "none since v1.6"}
+          <div className="bg-gradient-to-r text-blue-800 font-bold text-opacity-60 flex items-center gap-2 rounded-2xl border-2 border-black border-opacity-30 from-blue-200 to-blue-100 px-3 py-1 text-sm dark:text-blue-600 dark:from-blue-900 dark:to-blue-800">
+            <i className="text-blue-800 dark:text-blue-600 opacity-60 dark:opacity-100 fa-solid fa-globe"></i>
+            {themes.domain || `No domain`}
           </div>
         </div>
       </div>
